12e45b23c4fef34512b25d9ae814ae3c142b56bf
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2015-07-06 14:15:51 +0200

    initial check-in

---
+++ app.js
@@ -0,0 +1,34 @@
+/**
+ * Created by m.van.es on 6-7-2015.
+ */
+/* jshint node:true */
+'use strict';
+
+var express = require('express');
+var app = express();
+
+/* start: broadcast module */
+app.get('/broadcast/start', function (req, res) {
+    res.send('broadcast start');
+});
+
+app.get('/broadcast/stop', function (req, res) {
+    res.send('broadcast stop');
+});
+
+app.get('/broadcast/status', function (req, res) {
+    console.log('call to http://%s:%s/broadcast/status');
+    res.send('running');
+});
+/* end: broadcast module */
+
+app.use(express.static('public'));
+
+var server = app.listen(3000, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+
+});
