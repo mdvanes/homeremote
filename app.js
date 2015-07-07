86516a919a16d14b7180fd1b3c04cb7e2e34cd34
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2015-07-07 07:33:56 +0200

    added debug mode

--- app.js
+++ app.js
@@ -5,20 +5,69 @@
 'use strict';
 
 var express = require('express');
+var exec = require('child_process').exec;
 var app = express();
 
-/* start: broadcast module */
+var debug = false;
+
+// TODO debug mode that works on Windows
+// Detect debug mode
+process.argv.forEach(function (val, index) {
+    if(index === 2 && val === '--debugremote') {
+        console.warn('Running in debug mode!');
+        debug = true;
+    }
+});
+
+
+/* start: broadcast module TODO convert to real module */
 app.get('/broadcast/start', function (req, res) {
     console.log('call to http://%s:%s/broadcast/start');
-    res.send('ok');
+
+    if(debug) {
+        setTimeout(function() {
+            console.warn('debug: always return OK after 1 sec delay');
+            res.send('ok');
+        }, 1000);
+    } else {
+        setTimeout(function() {
+            exec('git config --global user.name', function(error, stdout, stderr){
+                console.log('['+stdout+']', stderr);
+                if(stdout.indexOf('m.van.es') > -1) {
+                    res.send('ok');
+                } else {
+                    res.send('error');
+                    //throw new Error('unexpected response');
+                }
+            });
+        }, 1000);
+    }
 });
 
 app.get('/broadcast/stop', function (req, res) {
-    res.send('broadcast stop');
+    console.log('call to http://%s:%s/broadcast/stop');
+
+    exec('git config --global user.name', function(error, stdout, stderr){
+        console.log('['+stdout+']', stderr);
+        if(stdout.indexOf('m.van.es') > -1) {
+            res.send('ok');
+        } else {
+            res.send('error');
+        }
+    });
 });
 
 app.get('/broadcast/status', function (req, res) {
     console.log('call to http://%s:%s/broadcast/status');
+
+    // cmd: sudo service broadcastxxx status
+    //
+    // options:
+    //    broadcastxxx stop/waiting                - when stopped, also the begin of the response of successful stop (multiline response)
+    //    broadcastxxx start/running, process 1111 - when started, pid is changed of course, also the response of successful start
+
+    // return: running or waiting
+
     res.send('stopped');
 });
 /* end: broadcast module */
@@ -30,6 +79,6 @@ var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log('HomeRemote listening at http://%s:%s', host, port);
 
 });
--- public/_js/core.js
+++ public/_js/core.js
@@ -4,32 +4,67 @@
     class BroadcastButton {
         constructor($elem) {
             this.$elem = $elem;
+            this.active = false;
             console.log('BroadcastButton', this.$elem);
 
             $.get('/broadcast/status')
                 .done((data) => {
                     if (data === 'running') {
+                        // TODO setActive
                         this.$elem.addClass('active');
-                    } else {
+                        this.active = true;
+                    } else if(data === 'waiting') {
+                        // TODO setInActive
                         this.$elem.removeClass('active');
+                    } else {
+                        // TODO error
                     }
                 }
             );
 
-            this.$elem.click(() =>
-                $.get('/broadcast/start'/*,
-                    {
+            this.$elem.click(() => this.click());
+        }
 
-                    }*/)
+        click() {
+            this.$elem.attr('class',''); // clear all classes
+            this.$elem.addClass('waiting');
+            // TODO or .error?
+            if(this.active) {
+                $.get('/broadcast/stop')
+                    .done((data) => this.handleStopResponse(data));
+            } else {
+                $.get('/broadcast/start'/*,
+                 {
+                 }*/)
                     .done((data) => {
+                        // TODO
                         if (data === 'ok') {
-                            this.$elem.addClass('active');
+                            this.setActive();
                         } else {
+                            this.$elem.attr('class',''); // clear all classes
+                            this.$elem.addClass('error');
                             alert('an error has occurred');
                         }
                     }
-                )
-            );
+                );
+            }
+        }
+
+        setActive() {
+            this.$elem.attr('class',''); // clear all classes
+            this.$elem.addClass('active');
+            this.active = true;
+        }
+
+        handleStopResponse(data) {
+            if (data === 'ok') {
+                this.$elem.attr('class',''); // clear all classes
+                this.active = false;
+            } else {
+                this.$elem.attr('class',''); // clear all classes
+                this.$elem.addClass('error');
+                alert('an error has occurred');
+            }
         }
     }
 
