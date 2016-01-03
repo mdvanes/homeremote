53efefaf7e146d086db7db7d3c95fd0f5e872dc4
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2016-01-03 14:10:23 +0100

    react test: basic controls

--- public/react/main.jsx
+++ public/react/main.jsx
@@ -3,16 +3,25 @@
 import React from 'react';
 import {render} from 'react-dom';
 ////import FilterableProductTable from './src/filterable-product-table';
-import MyRadio from './my-radio';
+import MyCheckbox from './my-checkbox';
+import MyLog from './my-log';
 
 // init shell
 renderShell();
 
 function renderShell() {
-    console.log('foo');
     var shell = document.createElement('div');
     shell.className = 'app-shell';
     document.body.appendChild(shell);
-    render(<MyRadio/>, shell);
+    render(
+        <div>
+            <MyCheckbox label="Switch 1" labelOn="lorem"/>
+            <MyCheckbox label="Switch 2" labelOff="ipsum"/>
+            <MyCheckbox label="Switch 3"/>
+            <MyCheckbox label="Switch 4"/>
+            <MyCheckbox label="Broadcast"/>
+            <MyCheckbox label="3FM on Server"/>
+            <MyLog/>
+        </div>, shell);
 }
 
