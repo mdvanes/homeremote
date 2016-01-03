e93ca7e71aebb32d0985a31972c9214d8d10f2eb
M.D. van Es <es@mdworld.nl>
2016-01-03 14:24:42 +0100

    react test: basic controls

--- public/react/main.jsx
+++ public/react/main.jsx
@@ -3,7 +3,8 @@
 import React from 'react';
 import {render} from 'react-dom';
 ////import FilterableProductTable from './src/filterable-product-table';
-import MyCheckbox from './my-checkbox';
+import MyButtonSet from './my-button-set';
+import MyToggle from './my-toggle';
 import MyLog from './my-log';
 
 // init shell
@@ -15,12 +16,12 @@ function renderShell() {
     document.body.appendChild(shell);
     render(
         <div>
-            <MyCheckbox label="Switch 1" labelOn="lorem"/>
-            <MyCheckbox label="Switch 2" labelOff="ipsum"/>
-            <MyCheckbox label="Switch 3"/>
-            <MyCheckbox label="Switch 4"/>
-            <MyCheckbox label="Broadcast"/>
-            <MyCheckbox label="3FM on Server"/>
+            <MyButtonSet label="Switch 1"/>
+            <MyToggle label="Switch 2"/>
+            <MyToggle label="Switch 3"/>
+            <MyToggle label="Switch 4"/>
+            <MyToggle label="Broadcast" labelOn="lorem"/>
+            <MyToggle label="3FM on Server" labelOff="ipsum"/>
             <MyLog/>
         </div>, shell);
 }
