d154ecb91b7fa2ed3eb6ea5cae35446579a84879
M.D. van Es <es@mdworld.nl>
2016-01-16 13:14:19 +0100

    react test: converted toggle to single button

--- public/react/components/toggle.jsx
+++ public/react/components/toggle.jsx
@@ -1,3 +1,86 @@
+import React from 'react';
+import $http from '../request';
+
+class MyToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isChecked: false};
+        $http('/togglestub/status')
+            .then(data => {
+                if(data.status === 'started') {
+                    this.setState({isChecked: true});
+                }
+            })
+            .catch(error => alert('error on toggle' + error));
+
+        this.onChange = this.onChange.bind(this);
+        this.sendToggle = this.sendToggle.bind(this);
+    }
+
+    onChange() {
+        console.log('onchange', this, this.state);
+        this.setState({isChecked: !this.state.isChecked});
+    }
+
+    sendToggle() {
+        let url = '/togglestub/';
+        if(this.state.isChecked) {
+            url += 'stop';
+        } else {
+            url += 'start';
+        }
+        $http(url)
+            .then(data => {
+                if(data.status === 'started') {
+                    this.setState({isChecked: true});
+                } else if(data.status === 'stopped') {
+                    this.setState({isChecked: false});
+                }
+            })
+            .catch(error => alert('error on toggle' + error));
+    }
+
+    //handleClickOff() {
+    //    this.setState({isChecked: false});
+    //}
+
+    render() {
+        //let classOn = 'btn btn-default';
+        //let classOff = 'btn btn-default';
+        //if(this.state.isChecked) {
+        //    classOn = 'btn btn-success';
+        //} else {
+        //    classOff = 'btn btn-danger';
+        //}
+        let buttonClass = 'btn btn-default';
+        if(this.state.isChecked) {
+            buttonClass = 'btn btn-success';
+        } else {
+            buttonClass = 'btn btn-danger';
+        }
+        return (
+            <div>
+                <label>
+                    {this.props.label}
+                    <input type="checkbox" checked={this.state.isChecked}
+                           onChange={this.onChange}/>
+                    {this.state.isChecked ? this.props.labelOn : this.props.labelOff}
+                </label>
+                {/*<div className="btn-group btn-group-justified">
+                    <a href="#" className={classOn} onClick={this.handleClickOn.bind(this)}>on</a> for this.onChange, the bind is done in the constructor
+                    <a href="#" className={classOff} onClick={this.handleClickOff.bind(this)}>off</a>
+                </div>*/}
+
+                <button className={buttonClass} onClick={this.sendToggle}>
+                    {this.props.label}
+                </button>
+                {/*<input type="Toggle" checked={this.state.isChecked} onChange={this.onChange}/>*/}
+            </div>
+        );
+    }
+}
+export default MyToggle;
+
 //import React from 'react';
 ////import ProductTable from './components/product-table';
 ////import SearchBar from './components/search-bar';
@@ -47,113 +130,3 @@
 //}
 //
 //export default MyRadio;
-
-
-
-
-
-import React from 'react';
-import $http from '../request';//
-
-class MyToggle extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {isChecked: false};
-        //var self = this;
-        $http('/togglestub/status')
-            .then(data => {
-                //console.log('togglestub status', data);
-                if(data.status === 'started') {
-                    //console.log('started', self.state, this.state);
-                    //self.state = {isChecked: true};
-                    this.setState({isChecked: true});
-                }
-            })
-            .catch(error => console.error('foobar', error));
-
-        this.onChange = this.onChange.bind(this);
-        //this.$http = this.$http.bind(this);
-    }
-
-    onChange() {
-        console.log('onchange', this, this.state);
-        this.setState({isChecked: !this.state.isChecked});
-    }
-
-    handleClickOn() {
-        //console.log('setting on', this);
-        //this.setState({isChecked: true});
-        //console.log('setting on2 ', this.state);
-        this.setState({isChecked: true});
-
-        $http('/togglestub/start')
-            .then(data => console.log('data', data))
-            .catch(error => console.error('foobar', error));
-    }
-
-    handleClickOff() {
-        this.setState({isChecked: false});
-    }
-
-    ///*
-    //Pure ES6, promise based, http requester
-    //
-    //Usage:
-    //
-    //$http('http://example.com/someurl/')
-    //    .then(data => {}})
-    //    .catch(error => {})
-    //*/
-    //$http(url) {
-    //    return new Promise(function(resolve, reject) {
-    //        // Standard XHR to load an image
-    //        var request = new XMLHttpRequest();
-    //        request.open('GET', url);
-    //        request.responseType = 'json';
-    //        // When the request loads, check whether it was successful
-    //        request.onload = function() {
-    //            if (request.status === 200) {
-    //                // If successful, resolve the promise by passing back the request response
-    //                //console.log('$http succesful response', request.response);
-    //                resolve(request.response);
-    //            } else {
-    //                // If it fails, reject the promise with a error message
-    //                reject(Error('URL didn\'t load successfully; error code:' + request.statusText));
-    //            }
-    //        };
-    //        request.onerror = function() {
-    //            // Also deal with the case when the entire request fails to begin with
-    //            // This is probably a network error, so reject the promise with an appropriate message
-    //            reject(Error('There was a network error.'));
-    //        };
-    //        // Send the request
-    //        request.send();
-    //    });
-    //}
-
-    render() {
-        let classOn = 'btn btn-default';
-        let classOff = 'btn btn-default';
-        if(this.state.isChecked) {
-            classOn = 'btn btn-success';
-        } else {
-            classOff = 'btn btn-danger';
-        }
-        return (
-            <div>
-                <label>
-                    {this.props.label}
-                    <input type="checkbox" checked={this.state.isChecked}
-                        onChange={this.onChange}/>
-                    {this.state.isChecked ? this.props.labelOn : this.props.labelOff}
-                </label>
-                <div className="btn-group btn-group-justified">
-                    <a href="#" className={classOn} onClick={this.handleClickOn.bind(this)}>on</a>{/* for this.onChange, the bind is done in the constructor */}
-                    <a href="#" className={classOff} onClick={this.handleClickOff.bind(this)}>off</a>
-                </div>
-                {/*<input type="Toggle" checked={this.state.isChecked} onChange={this.onChange}/>*/}
-            </div>
-        );
-    }
-}
-export default MyToggle;
