3b7b4f90411d9074e82d1ce1044b7250aaee0cd8
M.D. van Es <es@mdworld.nl>
2016-01-30 17:01:57 +0100

    react web: switch configurable

--- server/clickstub.js
+++ server/clickstub.js
@@ -1,5 +1,4 @@
-/* jshint node:true */
-'use strict';
+#!/usr/bin/env node
 
 var bind = function(app) {
     app.get('/clickstub/on', function (req, res) {
--- server/switch.js
+++ server/switch.js
@@ -1,20 +1,21 @@
-/* jshint node:true */
-'use strict';
+#!/usr/bin/env node
 
 var exec = require('child_process').exec;
 var fs = require('fs');
 var settings = require('../settings.json');
-//var settings = JSON.parse(fs.readFileSync('./settings.json'));
 
 var bind = function(app, log) {
-    app.get('/switch1/on', function (req, res) {
-        console.log('call to http://%s:%s/switch1/on --- ' + settings.hepath);
-        // Call should be: /home/martin/elro/he853-remote/he853 001 1
-        // TODO path to bin should be from server side configuration
-        // Expected result something like: Sending command[1] to deviceId[1]
 
-        exec('sudo ' + settings.hepath + '/he853 001 1', function(error, stdout, stderr){
+    app.get('/switch:id/:state', function (req, res) {
+        var switchId = req.params.id;
+        var switchState = req.params.state;
+
+        console.log('call to http://%s:%s/switch' + switchId + '/' + switchState + ' --- ' + settings.hepath + ' ' + switchId);
+
+        // Call should be: sudo /path/he853 001 1
+        exec('sudo ' + settings.hepath + '/he853 00' + switchId + ' ' + switchState, function(error, stdout, stderr){
             log.info('['+stdout+']');
+            // Expected result something like: Sending command[1] to deviceId[1]
             if(stdout.indexOf('Sending command[') > -1) {
                 res.send({status: 'received'});
             } else {
@@ -23,50 +24,6 @@ var bind = function(app, log) {
             }
         });
     });
-
-    app.get('/switch1/off', function (req, res) {
-        console.log('call to http://%s:%s/switch1/off');
-        exec('sudo ' + settings.hepath + '/he853 001 0', function(error, stdout, stderr){
-            log.info('['+stdout+']');
-            if(stdout.indexOf('Sending command[') > -1) {
-                res.send({status: 'received'});
-            } else {
-                log.error(stderr);
-                res.send({status: 'error'});
-            }
-        });
-    });
-
-    app.get('/switch2/on', function (req, res) {
-        console.log('call to http://%s:%s/switch2/on');
-        // Call should be: /home/martin/elro/he853-remote/he853 001 1
-        // TODO path to bin should be from server side configuration
-        // Expected result something like: Sending command[1] to deviceId[1]
-
-        exec('sudo /home/martin/elro/he853-remote/he853 002 1', function(error, stdout, stderr){
-            log.info('['+stdout+']');
-            if(stdout.indexOf('Sending command[') > -1) {
-                res.send({status: 'received'});
-            } else {
-                log.error(stderr);
-                res.send({status: 'error'});
-            }
-        });
-    });
-
-    app.get('/switch2/off', function (req, res) {
-        console.log('call to http://%s:%s/switch2/off');
-        exec('sudo /home/martin/elro/he853-remote/he853 002 0', function(error, stdout, stderr){
-            log.info('['+stdout+']');
-            if(stdout.indexOf('Sending command[') > -1) {
-                res.send({status: 'received'});
-            } else {
-                log.error(stderr);
-                res.send({status: 'error'});
-            }
-        });
-    });
-
 };
 
 module.exports = {
