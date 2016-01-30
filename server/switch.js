b18d6200d60e9e52a4be1e9eea68acde1562a219
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2016-01-30 16:20:36 +0100

    react web: added switch call

--- server/switch.js
+++ server/switch.js
@@ -2,10 +2,11 @@
 'use strict';
 
 var exec = require('child_process').exec;
+var settings = require('../settings.json');
 
 var bind = function(app, log) {
     app.get('/switch1/on', function (req, res) {
-        console.log('call to http://%s:%s/clickstub/on');
+        console.log('call to http://%s:%s/switch1/on');
         // Call should be: /home/martin/elro/he853-remote/he853 001 1
         // TODO path to bin should be from server side configuration
         // Expected result something like: Sending command[1] to deviceId[1]
@@ -22,6 +23,7 @@ var bind = function(app, log) {
     });
 
     app.get('/switch1/off', function (req, res) {
+        console.log('call to http://%s:%s/switch1/off');
         exec('sudo /home/martin/elro/he853-remote/he853 001 0', function(error, stdout, stderr){
             log.info('['+stdout+']');
             if(stdout.indexOf('Sending command[') > -1) {
@@ -32,6 +34,37 @@ var bind = function(app, log) {
             }
         });
     });
+
+    app.get('/switch2/on', function (req, res) {
+        console.log('call to http://%s:%s/switch2/on');
+        // Call should be: /home/martin/elro/he853-remote/he853 001 1
+        // TODO path to bin should be from server side configuration
+        // Expected result something like: Sending command[1] to deviceId[1]
+
+        exec('sudo /home/martin/elro/he853-remote/he853 002 1', function(error, stdout, stderr){
+            log.info('['+stdout+']');
+            if(stdout.indexOf('Sending command[') > -1) {
+                res.send({status: 'received'});
+            } else {
+                log.error(stderr);
+                res.send({status: 'error'});
+            }
+        });
+    });
+
+    app.get('/switch2/off', function (req, res) {
+        console.log('call to http://%s:%s/switch2/off');
+        exec('sudo /home/martin/elro/he853-remote/he853 002 0', function(error, stdout, stderr){
+            log.info('['+stdout+']');
+            if(stdout.indexOf('Sending command[') > -1) {
+                res.send({status: 'received'});
+            } else {
+                log.error(stderr);
+                res.send({status: 'error'});
+            }
+        });
+    });
+
 };
 
 module.exports = {
