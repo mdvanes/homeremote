c5f02c9f4a9b06cd9d3fd4ac200146c1e3dd0d21
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2016-01-30 19:48:34 +0100

    react web: added appcache

--- public/react/main.jsx
+++ public/react/main.jsx
@@ -1,4 +1,5 @@
 import {render} from 'react-dom';
+import React from 'react';
 import ButtonGroup from './components/button-group';
 import Toggle from './components/toggle';
 import Log from './components/log';
@@ -13,14 +14,11 @@ function renderShell() {
                 <div className="col-xs-12">
                     <ButtonGroup label=" 1" icon="lamp" id="switch1"/>
                     <ButtonGroup label=" 2" icon="lamp" id="switch2"/>
-                    <ButtonGroup label=" 3" icon="lamp" id="clickstub"/>
+                    <ButtonGroup label=" 3" icon="lamp" id="switch3"/>
                     <ButtonGroup label=" 4" icon="lamp" id="clickstub"/>
                 </div>
             </div>
             <div className="row margin-top">
-                {/*<Toggle label=" 2" id="togglestub" icon="lamp"/>
-                <Toggle label=" 3" id="togglestub" icon="lamp"/>
-                <Toggle label="Switch 4" id="togglestub"/>*/}
                 <Toggle label="" id="togglestub" icon="volume-up"/>{/* Broadcast */}
                 <Toggle label="" id="togglestub" icon="music"/>{/* Music on server */}
             </div>
