59b6d057f4e5ee5d57b45332102ad8a2f6ebd8d5
M.D. van Es <es@mdworld.nl>
2016-02-13 14:11:35 +0100

    react web: fixed timestamp issue

--- public/react/logger.js
+++ public/react/logger.js
@@ -6,9 +6,7 @@ let logger = {};
 
 let writeLog = (...messages) => {
     let time = new Date();
-    let timestamp = time.getHours() + ':' +
-        time.getMinutes() + '.' +
-        time.getSeconds();
+    let timestamp = time.toTimeString().substring(0,8);
     document.querySelector('.log').value = timestamp + ' ' +
         messages.join(' ') + '\n' +
         document.querySelector('.log').value;
