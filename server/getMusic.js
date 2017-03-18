3b618d6f7deb5bc1d72e67dee5804fe4bf6404ee
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2017-03-18 21:23:31 +0100

    #2 added ID3 support

--- server/getMusic.js
+++ server/getMusic.js
@@ -3,8 +3,11 @@
 
 const youtubedl = require('youtube-dl');
 const settings = require('../settings.json');
+const id3 = require('id3-writer');
+const writer = new id3.Writer();
 
 // TODO note: ffmpeg/ffprobe is required as an OS dependency
+// TODO also eyeD3: sudo apt-get install eyed3
 
 // const fsp = require('fs-promise');
 // const downloadPromise = (url, artist, title) => {
@@ -48,9 +51,12 @@ const downloadPromise = (url, artist, title) => {
         if(artist && artist.length > 0 && title && title.length > 0) {
             fileName = `${artist} - ${title}`;
         }
-        const targetPath = `${settings.musicpath}/${fileName}`;
+        let targetPath = `${settings.musicpath}/${fileName}`;
         // First send to mp4, mp3 is created with --extract-audio
         const options = ['-o', targetPath + '.mp4', '--extract-audio', '--audio-format', 'mp3', '--audio-quality', '0'];
+        // Extension is needed for setting metadata
+        targetPath += '.mp3';
+        fileName += '.mp3';
         youtubedl.exec(url, options, (err, output) => {
             if(err) {
                 reject('youtubedl.exec failed:' + err);
@@ -59,6 +65,24 @@ const downloadPromise = (url, artist, title) => {
                 resolve({path: targetPath, fileName});
             }
         });
+    });
+};
+
+const setMetadataPromise = (path, fileName, artist, title, album) => {
+    return new Promise((resolve, reject) => {
+        if(artist && artist.length > 0 && title && title.length > 0) {
+            const file = new id3.File(path);
+            const meta = new id3.Meta({ artist, title, album });
+
+            writer.setFile(file).write(meta, function(err) {
+                if (err) {
+                    reject('set metadata failed: ' + err);
+                } else {
+                    console.log('id3 finished');
+                    resolve({fileName});
+                }
+            });
+        }
     });
 };
 
@@ -87,6 +111,7 @@ const bind = app => {
         console.log(`Trying getMusic <${req.body.url}, ${req.body.title}>`);
 
         downloadPromise(req.body.url, req.body.artist, req.body.title)
+        .then(data => setMetadataPromise(data.path, data.fileName, req.body.artist, req.body.title, req.body.album))
         .then(data => res.send({status: 'ok', fileName: data.fileName}))
         // TODO change metadata: https://www.npmjs.com/package/id3-writer
         .catch(err => {
