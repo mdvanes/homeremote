ea51c2523354369b4201bf902016fb3dcd0c78c2
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2017-04-11 10:53:36 +0200

    Lowered cyclomatic complexity

--- src/js/reducers/reducers.js
+++ src/js/reducers/reducers.js
@@ -1,42 +1,54 @@
-import { LOG_INFO, LOG_ERROR, HIDE_SHORT_MESSAGE, CLEAR_LOG } from '../actions/actions';
+import { LOG_INFO, LOG_ERROR, HIDE_SHORT_MESSAGE, CLEAR_LOG } from '../actions/actions'; // eslint-disable-line no-unused-vars
 import { combineReducers } from 'redux';
 
 const loglines = (state = [], action) => {
-    switch (action.type) {
-        case LOG_ERROR:
-        case LOG_INFO:
-            return [
-                ...state,
-                {
-                    message: action.message
-                }
-            ];
-        case CLEAR_LOG:
-            return [];
-        default:
-            return state;
+    // Too keep cyclomatic complexity low
+    const newLogState = [
+        ...state,
+        {
+            message: action.message
+        }
+    ];
+    const actionTypeMap = {
+        LOG_ERROR: newLogState,
+        LOG_INFO: newLogState,
+        CLEAR_LOG: []
+    };
+    if(actionTypeMap.hasOwnProperty(action.type)) {
+        return actionTypeMap[action.type];
     }
+    return state;
+    // switch (action.type) {
+    //     case LOG_ERROR:
+    //     case LOG_INFO:
+    //         return [
+    //             ...state,
+    //             {
+    //                 message: action.message
+    //             }
+    //         ];
+    //     case CLEAR_LOG:
+    //         return [];
+    //     default:
+    //         return state;
+    // }
 };
 
 const short = (state = { shortMessage: '', showShortMessage: false }, action) => {
-    switch(action.type) {
-        case LOG_ERROR:
-        case LOG_INFO:
-        case HIDE_SHORT_MESSAGE:
-            return {
-                shortMessage: action.shortMessage,
-                showShortMessage: action.showShortMessage
-            };
-        default:
-            return state;
+    const newLogState = {
+        shortMessage: action.shortMessage,
+        showShortMessage: action.showShortMessage
+    };
+    const actionTypeMap = {
+        LOG_ERROR: newLogState,
+        LOG_INFO: newLogState,
+        HIDE_SHORT_MESSAGE: newLogState
+    };
+    if(actionTypeMap.hasOwnProperty(action.type)) {
+        return actionTypeMap[action.type];
     }
+    return state;
 };
-
-// export default function logApp(state = {}, action) {
-//     return {
-//         log: logReducer(state.log, action)
-//     };
-// }
 
 const logApp = combineReducers({
     loglines,
