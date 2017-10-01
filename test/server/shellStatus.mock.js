82cb1b6c5884d0ed42cc24817cbc5c2c8629ce8b
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2017-10-01 13:51:43 +0200

    mockatoo shellstatus

--- test/server/shellStatus.mock.js
+++ test/server/shellStatus.mock.js
@@ -9,7 +9,7 @@ module.exports = {
                 if(cmd === 'foo/1') {
                     cb(null, '{"bar": "baz", "bat": "man"}');
                 } else {
-                    cb(null, '');
+                    cb('Connection refused', '', 'Connection refused');
                 }
             }
         }
