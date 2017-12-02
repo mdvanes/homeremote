fafe2b25b023517760f429b740ad529cfda25ba1
M.D. van Es <es@mdworld.nl>
2017-12-02 15:06:22 +0100

    Separated log to view and Lights view is now the default

--- src/js/views/LogView.jsx
+++ src/js/views/LogView.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import GetMusic from '../containers/GetMusicContainer';
+import LogContainer from '../containers/LogContainer';
 
-export default class MusicView extends React.Component {
+export default class LogView extends React.Component {
     render() {
         return (
             <div className="col-xs-12 col-md-3 margin-top">
-                <GetMusic/>
+                <LogContainer />
             </div>
         );
     }
