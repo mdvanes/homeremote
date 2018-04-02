d526f16c7a84bf7e286e02eb0ef48cb32a7578dd
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2018-04-02 17:42:34 +0200

    Bump version

--- buildPatchExpressWs.sh
+++ buildPatchExpressWs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf node_modules/express-ws
 cd node_modules
-git clone git@github.com:mdvanes/express-ws.git
+git clone https://github.com/mdvanes/express-ws.git
 cd express-ws
 npm i
 ./node_modules/.bin/babel ../../src/ -d ../../lib/
