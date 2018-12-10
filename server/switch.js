be192e3add05877cfa7bf240403ca5e11181ddc0
M.D. van Es <es@mdworld.nl>
2018-12-10 19:27:12 +0100

    Get a list of switches from Domoticz

--- server/switch.js
+++ server/switch.js
@@ -7,7 +7,38 @@ const connectEnsureLogin = require('connect-ensure-login').ensureLoggedIn;
 
 const bind = function(app, log) {
 
-    app.post('/switch/:id', connectEnsureLogin(), function (req, res) {
+    app.get('/switches', connectEnsureLogin(), (req, res) => {
+        log.info(`Call to http://${req.get('host')}/switches domoticzuri: ${settings.domoticzuri}]`);
+        if(settings.domoticzuri && settings.domoticzuri.length > 0) {
+            const targetUri = `${settings.domoticzuri}/json.htm?type=devices&used=true&filter=all&favorite=1`;
+            rp(targetUri)
+              .then(function (remoteResponse) {
+                  const remoteResponseJson = JSON.parse(remoteResponse);
+                  if(remoteResponseJson.status === 'OK') {
+                      const switches = remoteResponseJson.result.map(item => ({
+                          idx: item.idx,
+                          type: item.Type,
+                          name: item.Name
+                      }));
+                      res.send({
+                          status: 'received',
+                          switches
+                      });
+                  } else {
+                      throw new Error('remoteResponse failed');
+                  }
+              })
+              .catch(function (err) {
+                  log.error(err);
+                  res.send({status: 'error'});
+              });
+        } else {
+            log.error('domoticzuri not configured');
+            res.send({status: 'error'});
+        }
+    });
+
+    app.post('/switch/:id', connectEnsureLogin(), (req, res) => {
         const switchId = req.params.id;
         const switchType = req.body.type;
         let newState = 'Off';
