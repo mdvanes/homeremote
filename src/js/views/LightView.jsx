125b04ce71e9108c38db9a6904c0aa4770f920f8
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2018-12-11 20:02:55 +0100

    Add new switches to dashboard

--- src/js/views/LightView.jsx
+++ src/js/views/LightView.jsx
@@ -5,25 +5,25 @@ import {Card, CardText} from 'material-ui/Card';
 import SwitchesListContainer from '../containers/SwitchesListContainer';
 
 const LightView = () => {
-    return (
-      <div className="col-xs-12 col-md-2"
-           style={{backgroundColor: '#e0e0e0', paddingBottom: '1em', paddingTop: '1em', height: '100vh'}}>
-        <SwitchesListContainer />
-        <Card style={{marginTop: '0.5em'}}>
-          <CardText className="row">
-            <div className="col-xs-4">
-              <ToggleContainer confirm={false} label="" id="radio" icon="music_note"/>{/* Music on server */}
-            </div>
-            <div className="col-xs-4">
-              <ToggleContainer confirm={false} label="" id="motion" icon="videocam"/>{/* Webcam/motion on server */}
-            </div>
-            <div className="col-xs-4">
-              <ToggleContainer confirm={true} label="" id="vm" icon="computer"/>{/* VM on server */}
-            </div>
-          </CardText>
-        </Card>
-      </div>
-    );
+  return (
+    <div className="col-xs-12 col-md-2"
+         style={{backgroundColor: '#e0e0e0', paddingBottom: '1em', paddingTop: '1em', height: '100vh'}}>
+      <SwitchesListContainer/>
+      <Card style={{marginTop: '0.5em'}}>
+        <CardText className="row">
+          <div className="col-xs-4">
+            <ToggleContainer confirm={false} label="" id="radio" icon="music_note"/>{/* Music on server */}
+          </div>
+          <div className="col-xs-4">
+            <ToggleContainer confirm={false} label="" id="motion" icon="videocam"/>{/* Webcam/motion on server */}
+          </div>
+          <div className="col-xs-4">
+            <ToggleContainer confirm={true} label="" id="vm" icon="computer"/>{/* VM on server */}
+          </div>
+        </CardText>
+      </Card>
+    </div>
+  );
 };
 
 export default LightView;
