eed6f5907134b92e0b9c570c3e9fa4c9e070ef52
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2018-12-23 11:17:52 +0100

    Clean up, return parsable error code

--- server/serviceToggle.js
+++ server/serviceToggle.js
@@ -12,7 +12,7 @@ const connectEnsureLogin = require('connect-ensure-login').ensureLoggedIn;
 const execPromise = startCmd => (
   new Promise((resolve, reject) => (
     exec(startCmd, (error, stdout, stderr) => {
-      // error.code = 3 when service "Active: inactive (dead)" or "Active: failed"
+      // Note: error.code = 3 when service "Active: inactive (dead)" or "Active: failed"
       if(error && error.code > 0 && error.code !== 3) {
         reject(`${error.code} ${stdout} ${stderr}`);
       } else {
@@ -29,11 +29,6 @@ async function bindAction(app, endpointName, log, settings, type, statusCode, ac
       log.info(`call to /${endpointName}/${type}`);
       const {stdout, exitCode} = await execPromise(settings[type]);
       log.info(`/${endpointName}/${type} stdout [${stdout}] [${exitCode}]`);
-      // if(stdout.length === 0) {
-      //   res.send({status: statusCode});
-      // } else {
-      //   throw new Error('unexpected');
-      // }
       // Abstract action selection that scales to unlimited conditions
       const selectedAction = actions
         .filter(action => action.condition(stdout))
@@ -47,43 +42,10 @@ async function bindAction(app, endpointName, log, settings, type, statusCode, ac
       selectedAction(res, statusCode);
     } catch(err) {
       log.error(`Failed ${endpointName} ${type}`, err);
-      res.send('error');
+      res.send({status: 'error'});
     }
   })
 }
-
-//   (app
-//   .get(`/${endpointName}/start`, connectEnsureLogin(), (req, res) => {
-//     log.info(`call to /${endpointName}/start`);
-//
-//     exec(settings.start, (error, stdout, stderr) => {
-//         log.info('['+stdout+']');
-//         // TODO Upstart gave more information when starting/stopping, do a better check for systemd
-//         //if(stdout.indexOf('playradio start/') > -1) {
-//         if(stdout.length === 0) {
-//             res.send({status: 'started'});
-//         } else {
-//             log.error(error + ' ' + stderr);
-//             res.send('error');
-//         }
-//     });
-// }));
-
-// app.get(settings.stop, connectEnsureLogin(), function (req, res) {
-//     log.info(`call to /${endpointName}/stop`);
-//     exec(`sudo service ${settings} stop`, function(error, stdout, stderr){
-//         log.info('['+stdout+']');
-//         // TODO Upstart gave more information when starting/stopping, do a better check for systemd
-//         //if(stdout.indexOf('playradio stop/') > -1) {
-//         if(stdout.length === 0) {
-//             res.send({status: 'stopped'});
-//         } else {
-//             log.error(error + ' ' + stderr);
-//             res.send('error');
-//         }
-//     });
-// });
-//
 
 /* Actions for start or stop */
 const startStopActions = [
@@ -124,22 +86,6 @@ const statusActions = [
     action: res => res.send({status: 'stopped'})
   }
 ];
-
-// app.get(`/${endpointName}/status`, connectEnsureLogin(), (req, res) => {
-//     log.info(`call to /${endpointName}/status`);
-//     exec(settings.status, (error, stdout, stderr) => {
-//         log.info('stdout: ['+stdout+']', isStartedCondition);
-//         if(stdout.indexOf(isStartedCondition) > -1) {
-//             console.log('IS STARTED')
-//             res.send({status: 'started'});
-//         } else if(isStopped(stdout)) {
-//             res.send({status: 'stopped'});
-//         } else {
-//             log.error(error + ' ' + stdout + '|' + stderr);
-//             res.send('error');
-//         }
-//     });
-// });
 
 /**
  *  HOB - Higher Order Bind
@@ -189,29 +135,7 @@ const hob = settings => {
       statusActions
     );
   };
-
-  // const isStartedCondition = settings.isStarted
-  //   ? settings.isStarted
-  //   : 'Active: active'
-  // ;
-  //
-  // const isStopped = stdout => (settings.isStopped
-  //   ? stdout.indexOf(settings.isStopped[0]) > -1 || stdout.indexOf(settings.isStopped[0]) > -1
-  //   : stdout.indexOf('Active: failed') > -1 || stdout.indexOf('Active: inactive') > -1
-  // );
-
-  // {
-  //   return settings.isStopped ? 1 : 0;
-  //   // return settings.isStopped
-  //   //   ? stdout.indexOf('powered off (') > -1 || stdout.indexOf('aborted ('
-  //   //   : stdout.indexOf('Active: failed') > -1 || stdout.indexOf('Active: inactive') > -1;
-  // }
 };
-
-// TODO needs cleaning and tests
-
-// TODO test on dev machine without mock -- apparently very difficult...
-// Status handeling for "stopped" is incorrect, but correct for "started" and only for services, not vm
 
 module.exports = {
   hob,
