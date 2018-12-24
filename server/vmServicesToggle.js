c77212e47851d6b1b40fb9b92fca062776ef45fc
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2018-12-24 14:01:28 +0100

    Fix service elevation

--- server/vmServicesToggle.js
+++ server/vmServicesToggle.js
@@ -6,9 +6,9 @@ const settings = require('../settings.json');
 
 const bind = function(app, log) {
   const bindToggle = hobToggle.hob({
-    start: `ssh -p ${settings.vmservices.port} ${settings.vmservices.user}@${settings.vmservices.host} '~/startServices.sh'`,
-    stop: `ssh -p ${settings.vmservices.port} ${settings.vmservices.user}@${settings.vmservices.host} '~/stopServices.sh'`,
-    status: `ssh -p ${settings.vmservices.port} ${settings.vmservices.user}@${settings.vmservices.host} '~/statusServices.sh'`,
+    start: `${settings.vmservices.elevate} ssh -p ${settings.vmservices.port} ${settings.vmservices.user}@${settings.vmservices.host} '~/startServices.sh'`,
+    stop: `${settings.vmservices.elevate} ssh -p ${settings.vmservices.port} ${settings.vmservices.user}@${settings.vmservices.host} '~/stopServices.sh'`,
+    status: `${settings.vmservices.elevate} ssh -p ${settings.vmservices.port} ${settings.vmservices.user}@${settings.vmservices.host} '~/statusServices.sh'`,
   });
   bindToggle(app, 'vmservices', log);
 };
