8b114fb9f1d9d54a89e8d4ae1c78f9dbeeaa304d
M.D. van Es <es@mdworld.nl>
2018-12-24 08:15:18 +0100

    Extracted vmServicesToggle

---
+++ server/vmServicesToggle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+/* eslint-env node */
+
+const hobToggle = require('./hobToggle');
+const settings = require('../settings.json');
+
+const bind = function(app, log) {
+  const bindToggle = hobToggle.hob({
+    start: `ssh -p ${settings.vmservices.port} ${settings.vmservices.user}@${settings.vmservices.host} '~/startServices.sh'`,
+    stop: `ssh -p ${settings.vmservices.port} ${settings.vmservices.user}@${settings.vmservices.host} '~/stopServices.sh'`,
+    status: `ssh -p ${settings.vmservices.port} ${settings.vmservices.user}@${settings.vmservices.host} '~/statusServices.sh'`,
+  });
+  bindToggle(app, 'vm', log);
+};
+
+module.exports = { bind };
