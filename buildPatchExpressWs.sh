9d38d7a04bf0cd0317735ffa8cdb6c2b2a91437f
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-02-23 09:50:33 +0100

    Clear front-end sources for migration to CRA

--- buildPatchExpressWs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -rf node_modules/express-ws
-cd node_modules
-git clone https://github.com/mdvanes/express-ws.git
-cd express-ws
-npm i
-./node_modules/.bin/babel ../../src/ -d ../../lib/
-cd ..
