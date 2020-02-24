534d4e3eed69897fa9b34a7b23639816dc29601c
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-02-24 19:22:10 +0100

    Add material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,39 @@ import React from 'react';
 // import logo from './logo.svg';
 // Example of importing CSS
 import './App.css';
+import {AppBar, createMuiTheme, Drawer, IconButton, MuiThemeProvider, Toolbar,  Typography} from "@material-ui/core";
+import {deepPurple, orange} from "@material-ui/core/colors";
+import MenuIcon from '@material-ui/icons/Menu';
+import HomeAutomation from "./Components/Pages/HomeAutomation/HomeAutomation";
+
+const theme = createMuiTheme({
+  palette: {
+    // Can't use deepPurple[900] directly, see https://material-ui.com/customization/color/#official-color-tool
+    primary: deepPurple,
+    secondary: orange
+  }
+});
 
 function App() {
   return (
-    <div className="App">
+    <MuiThemeProvider theme={theme}>
+      <AppBar>
+        <Toolbar>
+          <IconButton edge="start"  color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">
+            HomeRemote
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer>
+        <AppBar></AppBar>
+        test
+      </Drawer>
+      <HomeAutomation />
+    </MuiThemeProvider>
+/*    <div className="App">
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -20,7 +49,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>*/
   );
 }
 
---
+++ src/Components/Pages/HomeAutomation/HomeAutomation.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function HomeAutomation() {
+  return (
+    <div style={{ marginTop: "100px"}}>HomeAutomation</div>
+  );
+}
+
+export default HomeAutomation;
