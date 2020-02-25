91e193872ccfd923f7026f15a79d68e5646d4877
M.D. van Es <es@mdworld.nl>
2020-02-25 18:38:10 +0100

    Add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,47 @@
-import React from 'react';
+import React, { FC } from 'react';
 // Example of importing image
 // import logo from './logo.svg';
 // Example of importing CSS
 import './App.css';
-import {AppBar, createMuiTheme, Drawer, IconButton, MuiThemeProvider, Toolbar,  Typography} from "@material-ui/core";
-import {deepPurple, orange} from "@material-ui/core/colors";
+import {
+    AppBar,
+    createMuiTheme,
+    Drawer,
+    IconButton,
+    MuiThemeProvider,
+    Toolbar,
+    Typography
+} from '@material-ui/core';
+import { deepPurple, orange } from '@material-ui/core/colors';
 import MenuIcon from '@material-ui/icons/Menu';
-import HomeAutomation from "./Components/Pages/HomeAutomation/HomeAutomation";
+import HomeAutomation from './Components/Pages/HomeAutomation/HomeAutomation';
 
 const theme = createMuiTheme({
-  palette: {
-    // Can't use deepPurple[900] directly, see https://material-ui.com/customization/color/#official-color-tool
-    primary: deepPurple,
-    secondary: orange
-  }
+    palette: {
+        // Can't use deepPurple[900] directly, see https://material-ui.com/customization/color/#official-color-tool
+        primary: deepPurple,
+        secondary: orange
+    }
 });
 
-function App() {
-  return (
-    <MuiThemeProvider theme={theme}>
-      <AppBar>
-        <Toolbar>
-          <IconButton edge="start"  color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6">
-            HomeRemote
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer>
-        <AppBar></AppBar>
-        test
-      </Drawer>
-      <HomeAutomation />
-    </MuiThemeProvider>
-/*    <div className="App">
+const App: FC = () => {
+    return (
+        <MuiThemeProvider theme={theme}>
+            <AppBar>
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6">HomeRemote</Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer>
+                <AppBar></AppBar>
+                test
+            </Drawer>
+            <HomeAutomation />
+        </MuiThemeProvider>
+        /*    <div className="App">
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -50,7 +56,7 @@ function App() {
         </a>
       </header>
     </div>*/
-  );
-}
+    );
+};
 
 export default App;
--- src/Components/Pages/HomeAutomation/HomeAutomation.tsx
+++ src/Components/Pages/HomeAutomation/HomeAutomation.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { FC } from 'react';
 
-function HomeAutomation() {
-  return (
-    <div style={{ marginTop: "100px"}}>HomeAutomation</div>
-  );
-}
+const HomeAutomation: FC = () => {
+    return <div style={{ marginTop: '100px' }}>HomeAutomation</div>;
+};
 
 export default HomeAutomation;
