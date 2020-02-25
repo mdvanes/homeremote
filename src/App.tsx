87757d5c68e8149758d51396aef10bc4b2ac13ba
M.D. van Es <es@mdworld.nl>
2020-02-25 18:57:06 +0100

    Add drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 // Example of importing image
 // import logo from './logo.svg';
 // Example of importing CSS
@@ -10,7 +10,8 @@ import {
     IconButton,
     MuiThemeProvider,
     Toolbar,
-    Typography
+    Typography,
+    Link, MenuItem
 } from '@material-ui/core';
 import { deepPurple, orange } from '@material-ui/core/colors';
 import MenuIcon from '@material-ui/icons/Menu';
@@ -25,21 +26,40 @@ const theme = createMuiTheme({
 });
 
 const App: FC = () => {
+    const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
+    const toggleDrawer = (): void => setIsDrawerOpen(!isDrawerOpen);
     return (
         <MuiThemeProvider theme={theme}>
             <AppBar>
                 <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        onClick={toggleDrawer}
+                    >
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6">HomeRemote</Typography>
                 </Toolbar>
             </AppBar>
-            <Drawer>
-                <AppBar></AppBar>
-                test
+            <Drawer
+                open={isDrawerOpen}
+                onClose={(): void => setIsDrawerOpen(false)}
+            >
+                <MenuItem>Home Automation</MenuItem>
+                {/*<Link onClick={this.toggleDrawer} to="/"><MenuItem>Home Automation</MenuItem></Link>
+                <Link to="/r/music"><MenuItem>Music</MenuItem></Link>
+                <Link to="/r/files"><MenuItem>Files</MenuItem></Link>
+                <Link to="/r/gears"><MenuItem>Gears</MenuItem></Link>
+                <Link to="/r/log"><MenuItem>Log</MenuItem></Link>
+                <Link to="/r/dashboard"><MenuItem>Dashboard</MenuItem></Link>
+                <a onClick={() => { localStorage.removeItem('hr-remember-me') }} href="logout"><MenuItem>Log out</MenuItem></a>*/}
             </Drawer>
+            {/* TODO what is this? <StatusBar/>*/}
             <HomeAutomation />
+            test
+            {/* TODO <GlobalSnackBar/>*/}
         </MuiThemeProvider>
         /*    <div className="App">
       <header className="App-header">
