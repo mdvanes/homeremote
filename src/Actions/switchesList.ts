94491ad6a5b4082e633d9753630674fc7fb01877
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-02-26 20:40:55 +0100

    Add redux

---
+++ src/Actions/switchesList.ts
@@ -0,0 +1,8 @@
+export const SET_SWITCHES = 'SET_SWITCHES';
+
+export const setSwitches = (switches: any) => {
+    return {
+        type: SET_SWITCHES,
+        switches
+    };
+};
---
+++ src/Reducers/index.ts
@@ -0,0 +1,20 @@
+// import loglines from './loglines';
+// import short from './short';
+// import moveParams from './moveParams';
+// import moveProgress from './moveProgress';
+// import fileManager from './fileManager';
+import switchesList from './switchesList';
+// import expandedScenes from './expandedScenes';
+import { combineReducers } from 'redux';
+
+const homeRemoteReducers = combineReducers({
+    // loglines,
+    // short,
+    // moveParams,
+    // moveProgress,
+    // fileManager,
+    switchesList
+    // expandedScenes
+});
+
+export default homeRemoteReducers;
