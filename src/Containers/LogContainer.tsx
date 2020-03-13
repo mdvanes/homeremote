8280489b819b69eb97bb36d920a48f9e5add25a1
M.D. van Es <es@mdworld.nl>
2020-03-13 15:07:31 +0100

    Add LogContainer types

--- src/Containers/LogContainer.tsx
+++ src/Containers/LogContainer.tsx
@@ -1,24 +1,57 @@
 import { connect } from 'react-redux';
 import { logInfo, logError } from '../Actions';
 import Log from '../Components/Molecules/Log/Log';
+import { Dispatch } from 'redux';
 
-const mapStateToProps = (state: any) => {
+type Logline = {
+    message: string;
+};
+
+type State = {
+    loglines: Logline[];
+};
+
+// And use this in ../Components/Molecules/Log/Log
+type StateProps = State;
+
+const mapStateToProps = (state: State): StateProps => {
     return {
         loglines: state.loglines
     };
 };
 
-const mapDispatchToProps = (dispatch: any) => {
+// And use this in ../Components/Molecules/Log/Log
+type ActionProps = {
+    logInfo: (...messages: string[]) => void;
+    logError: (...messages: string[]) => void;
+};
+
+// TODO How to type bindActionCreators?
+// const mapDispatchToProps = (dispatch: Dispatch): ActionProps => {
+//     return bindActionCreators(
+//         {
+//             logInfo: (...messages: any) => {
+//                 dispatch(logInfo(...messages));
+//             },
+//             logError: (...messages: any) => {
+//                 dispatch(logError(...messages));
+//             }
+//         },
+//         dispatch
+//     );
+// };
+const mapDispatchToProps = (dispatch: Dispatch): ActionProps => {
     return {
-        logInfo: (...messages: any) => {
+        logInfo: (...messages: string[]): void => {
             dispatch(logInfo(...messages));
         },
-        logError: (...messages: any) => {
+        logError: (...messages: string[]): void => {
             dispatch(logError(...messages));
         }
     };
 };
 
+// TODO add generic types to connect?
 const LogContainer = connect(mapStateToProps, mapDispatchToProps)(Log);
 
 export default LogContainer;
