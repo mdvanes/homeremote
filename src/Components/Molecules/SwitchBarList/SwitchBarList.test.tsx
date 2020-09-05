c379d7be43b067c4f155edae62c08df91a2206c6
M.D. van Es <es@mdworld.nl>
2020-09-05 17:22:12 +0200

    Fix unit tests

--- src/Components/Molecules/SwitchBarList/SwitchBarList.test.tsx
+++ src/Components/Molecules/SwitchBarList/SwitchBarList.test.tsx
@@ -1,18 +1,36 @@
+import React from "react";
 import * as ReactRedux from "react-redux";
 import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 import SwitchBarList from "./SwitchBarList";
 import { RootState } from "../../../Reducers";
 
 describe("SwitchBarList", () => {
-    it("loads", () => {
+    const mockDispatch = jest.fn();
+    it("show a normal light switch", () => {
         jest.spyOn(ReactRedux, "useSelector").mockReset();
         jest.spyOn(ReactRedux, "useSelector").mockImplementation(fn => {
             const mockRootState: RootState = {
-                slice1: {},
+                switchesList: {
+                    isLoading: false,
+                    error: false,
+                    switches: [
+                        {
+                            idx: "3",
+                            type: "Light/Switch",
+                            name: "My Normal Light Switch",
+                            status: "On",
+                            dimLevel: null,
+                            readOnly: false,
+                            children: false
+                        },
+                    ],
+                    expanded: [],
+                },
             };
             return fn(mockRootState);
         });
+        jest.spyOn(ReactRedux, "useDispatch").mockReturnValue(mockDispatch);
         const { getByText } = render(<SwitchBarList />);
-        expect(getByText("hoi")).toBeInTheDocument();
+        expect(getByText("My Normal Light Switch")).toBeInTheDocument();
     });
 });
