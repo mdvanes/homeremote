eb708839fd29d618b8a76be8ad3be56d74f9ab8c
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-09-06 09:13:50 +0200

    Write git info to other dir

--- writeGitInfo.js
+++ writeGitInfo.js
@@ -1,16 +1,16 @@
-const fs = require('fs');
-const git = require('git-rev-sync');
+const fs = require("fs");
+const git = require("git-rev-sync");
 
-const DEV = 'DEV';
-const PROD = 'PROD';
+const DEV = "DEV";
+const PROD = "PROD";
 
-const mode = process.argv.length >= 3 && process.argv[2] === 'dev' ? DEV : PROD;
+const mode = process.argv.length >= 3 && process.argv[2] === "dev" ? DEV : PROD;
 
 const json = JSON.stringify({
-  hash: mode === DEV ? 'development' : git.short(),
-  branch: git.branch()
+    hash: mode === DEV ? "development" : git.short(),
+    branch: git.branch(),
 });
 
 console.log(`Setting gitinfo.json with ${json}`);
 
-fs.writeFile('gitinfo.json', json, 'utf8', () => {});
+fs.writeFile("src/gitinfo.json", json, "utf8", () => {});
