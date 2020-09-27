1e1cf2cf380fa8eff5862e308cec655679a25755
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-09-27 16:47:24 +0200

    Add unit tests for pages

---
+++ src/Components/Pages/Log/Log.test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Log from "./Log";
+
+jest.mock("../../Molecules/LogCard/LogCard", () => "mock-log-card");
+
+describe("Log page", () => {
+    it("contains the LogCard", () => {
+        const { baseElement } = render(<Log />);
+        expect(baseElement).toMatchSnapshot();
+    });
+});
