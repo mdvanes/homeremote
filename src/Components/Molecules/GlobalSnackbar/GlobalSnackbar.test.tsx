5bbcc5f54a428c2ac355baa4863fce623c1dcabf
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-10-03 11:55:01 +0200

    Add GlobalSnackbar unit tests

---
+++ src/Components/Molecules/GlobalSnackbar/GlobalSnackbar.test.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import * as ReactRedux from "react-redux";
+import { fireEvent, render, wait } from "@testing-library/react";
+import GlobalSnackbar from "./GlobalSnackbar";
+
+const mockUseSelector = jest.spyOn(ReactRedux, "useSelector");
+
+describe("GlobalSnackbar", () => {
+    it("adds a Snackbar to the page", () => {
+        mockUseSelector.mockReset();
+        mockUseSelector.mockImplementation(fn => {
+            return fn({
+                loglines: {
+                    urgentMessage: "some string",
+                },
+            });
+        });
+        const { getByTestId } = render(<GlobalSnackbar />);
+        expect(getByTestId("global-snackbar")).toBeInTheDocument();
+    });
+
+    it("closes Snackbar on close button", async () => {
+        mockUseSelector.mockReset();
+        mockUseSelector.mockImplementation(fn => {
+            return fn({
+                loglines: {
+                    urgentMessage: "some string",
+                },
+            });
+        });
+        const { baseElement, queryByTestId } = render(<GlobalSnackbar />);
+        const closeButton = baseElement.querySelector("button");
+        if (closeButton) {
+            fireEvent.click(closeButton);
+        }
+        await wait(
+            () => {
+                expect(
+                    queryByTestId("global-snackbar")
+                ).not.toBeInTheDocument();
+            },
+            { timeout: 1000 } // Should be removed, before the auto-healing feature of Snackbar removes it
+        );
+    });
+
+    it("does not show Snackbar if there is no urgentMessage", () => {
+        mockUseSelector.mockReset();
+        mockUseSelector.mockImplementation(fn => {
+            return fn({
+                loglines: {},
+            });
+        });
+        const { queryByTestId } = render(<GlobalSnackbar />);
+        expect(queryByTestId("global-snackbar")).not.toBeInTheDocument();
+    });
+});
