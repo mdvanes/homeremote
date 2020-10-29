734fcc9a283d0568ae3daffed73a9394f3bc536b
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-10-29 19:32:14 +0100

    Improve unit tests

--- src/testHelpers.tsx
+++ src/testHelpers.tsx
@@ -1,15 +1,5 @@
 import React, { FC, ReactElement } from "react";
-import * as ReactRedux from "react-redux";
-import {
-    render,
-    fireEvent,
-    RenderResult,
-    RenderOptions,
-} from "@testing-library/react";
-// import SwitchBarList from "./SwitchBarList";
-// import { RootState } from "../../../Reducers";
-// import * as Slice from "./switchBarListSlice";
-// import SwitchBar from "./SwitchBar";
+import { render, RenderResult, RenderOptions } from "@testing-library/react";
 import { Provider } from "react-redux";
 import {
     combineReducers,
@@ -21,7 +11,7 @@ import { RootState } from "./Reducers";
 
 interface RenderWithProvidersOptions extends RenderOptions {
     initialState: Partial<RootState>;
-    reducers: Record<string, Reducer>; // Reducer<CombinedState<>, AnyAction>;
+    reducers: Record<string, Reducer>;
     store?: Store;
 }
 
@@ -30,14 +20,12 @@ type RenderWithProviders = (
     options: RenderWithProvidersOptions
 ) => RenderResult;
 
-// TODO extract to helper
 export const renderWithProviders: RenderWithProviders = (
     ui,
     {
         initialState,
         reducers,
         store = configureStore({
-            // reducer: combineReducers({ switchesList: Slice.default }),
             reducer: combineReducers(reducers),
             preloadedState: initialState,
         }),
