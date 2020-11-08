192956670a67998668d3fd0ff73c9836667e1a32
M.D. van Es <es@mdworld.nl>
2020-11-08 16:25:55 +0100

    Add app status slice

--- src/Reducers/index.ts
+++ src/Reducers/index.ts
@@ -3,11 +3,13 @@ import loglinesReducer from "../Components/Molecules/LogCard/logSlice";
 // import moveParams from './moveParams';
 // import moveProgress from './moveProgress';
 // import fileManager from './fileManager';
+import appStatusReducer from "../Components/Molecules/AppStatusButton/appStatusSlice";
 import switchBarListReducer from "../Components/Molecules/SwitchBarList/switchBarListSlice";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
     loglines: loglinesReducer,
+    appStatus: appStatusReducer,
     switchesList: switchBarListReducer,
     // short,
     // moveParams,
