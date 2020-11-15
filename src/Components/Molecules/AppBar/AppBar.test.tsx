4dbd73f68c6cfc35111fab05e3357f590004e204
M.D. van Es <es@mdworld.nl>
2020-11-15 11:00:35 +0100

    Add unit test

--- src/Components/Molecules/AppBar/AppBar.test.tsx
+++ src/Components/Molecules/AppBar/AppBar.test.tsx
@@ -1,5 +1,30 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import AppBar from "./AppBar";
+
+jest.mock("../AppStatusButton/AppStatusButton", () => "mock-app-status-button");
+
+const mockToggleDrawer = jest.fn();
+
 describe("AppBar", () => {
-    it("???", () => {
-        /* TODO add tests */
+    it("has the application title", () => {
+        const { getByText } = render(
+            <AppBar toggleDrawer={mockToggleDrawer} />
+        );
+        expect(getByText("HomeRemote")).toBeInTheDocument();
+    });
+
+    it("opens the drawer when the menu button is clicked", () => {
+        mockToggleDrawer.mockReset();
+        const { baseElement } = render(
+            <AppBar toggleDrawer={mockToggleDrawer} />
+        );
+        const menuButton = baseElement.querySelector(
+            "button.makeStyles-menuButton"
+        );
+        if (menuButton) {
+            fireEvent.click(menuButton);
+        }
+        expect(mockToggleDrawer).toHaveBeenCalledTimes(1);
     });
 });
