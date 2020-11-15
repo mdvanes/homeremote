67b9183c7f3898ef7ed303d5003e8dc3970b8393
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-11-15 10:52:23 +0100

    Add unit tests

--- src/Components/Molecules/DrawerMenu/DrawerMenu.test.tsx
+++ src/Components/Molecules/DrawerMenu/DrawerMenu.test.tsx
@@ -1,5 +1,49 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import DrawerMenu from "./DrawerMenu";
+
+jest.mock("react-router-dom", () => ({
+    Link: "mock-link",
+}));
+
+const mockCloseDrawer = jest.fn();
+
 describe("DrawerMenu", () => {
-    it("???", () => {
-        /* TODO add tests */
+    beforeEach(() => {
+        mockCloseDrawer.mockReset();
+    });
+
+    it("has Dashboard link", () => {
+        const { getByText } = render(
+            <DrawerMenu closeDrawer={mockCloseDrawer} />
+        );
+        expect(getByText("Dashboard")).toBeInTheDocument();
+    });
+
+    it("closes the drawer after a menu item is clicked", () => {
+        const { getByText } = render(
+            <DrawerMenu closeDrawer={mockCloseDrawer} />
+        );
+        fireEvent.click(getByText("Dashboard"));
+        expect(mockCloseDrawer).toHaveBeenCalledTimes(1);
+    });
+
+    it("logouts on logout link click", () => {
+        interface MockWindow {
+            location?: Location;
+        }
+        const oldLocation = window.location;
+        delete (window as MockWindow).location;
+        window.location = {
+            ...oldLocation,
+            href: "/foo",
+        };
+        expect(window.location.href).toBe("/foo");
+        const { getByText } = render(
+            <DrawerMenu closeDrawer={mockCloseDrawer} />
+        );
+        fireEvent.click(getByText("Log out"));
+        expect(mockCloseDrawer).toHaveBeenCalledTimes(1);
+        expect(window.location.href).toBe("/");
     });
 });
--- src/Components/Molecules/DrawerMenu/DrawerMenu.tsx
+++ src/Components/Molecules/DrawerMenu/DrawerMenu.tsx
@@ -6,41 +6,42 @@ interface Props {
     closeDrawer: () => void;
 }
 
-const DrawerMenu: FC<Props> = ({ closeDrawer }) => {
-    return (
-        <div role="presentation" onClick={closeDrawer}>
-            <List>
-                <ListItem button component={RouterLink} to="/">
-                    <ListItemText primary="Home Automation" />
-                </ListItem>
-                <ListItem button component={RouterLink} to="/dashboard">
-                    <ListItemText primary="Dashboard" />
-                </ListItem>
-                <ListItem button component={RouterLink} to="/music">
-                    <ListItemText primary="Music" />
-                </ListItem>
-                <ListItem button component={RouterLink} to="/gears">
-                    <ListItemText primary="Gears" />
-                </ListItem>
-                <ListItem button component={RouterLink} to="/log">
-                    <ListItemText primary="Logging" />
-                </ListItem>
-                <ListItem button component={RouterLink} to="/streams">
-                    <ListItemText primary="Streams" />
-                </ListItem>
-                <ListItem
-                    button
-                    onClick={(): void => {
-                        // localStorage.removeItem("hr-remember-me");
-                        localStorage.removeItem("token");
-                        window.location.href = "/";
-                    }}
-                >
-                    <ListItemText primary="Log out" />
-                </ListItem>
-            </List>
-        </div>
-    );
-};
+// TODO indicate active route, see https://material-ui.com/guides/composition/#link
+
+const DrawerMenu: FC<Props> = ({ closeDrawer }) => (
+    <div role="presentation" onClick={closeDrawer}>
+        <List>
+            <ListItem button component={RouterLink} to="/">
+                <ListItemText primary="Home Automation" />
+            </ListItem>
+            <ListItem button component={RouterLink} to="/dashboard">
+                <ListItemText primary="Dashboard" />
+            </ListItem>
+            <ListItem button component={RouterLink} to="/music">
+                <ListItemText primary="Music" />
+            </ListItem>
+            <ListItem button component={RouterLink} to="/gears">
+                <ListItemText primary="Gears" />
+            </ListItem>
+            <ListItem button component={RouterLink} to="/log">
+                <ListItemText primary="Logging" />
+            </ListItem>
+            <ListItem button component={RouterLink} to="/streams">
+                <ListItemText primary="Streams" />
+            </ListItem>
+            <ListItem
+                button
+                onClick={(): void => {
+                    // TODO call logout endpoint
+                    // localStorage.removeItem("hr-remember-me");
+                    // localStorage.removeItem("token");
+                    window.location.href = "/";
+                }}
+            >
+                <ListItemText primary="Log out" />
+            </ListItem>
+        </List>
+    </div>
+);
 
 export default DrawerMenu;
