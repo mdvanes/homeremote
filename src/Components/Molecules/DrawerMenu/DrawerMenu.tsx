164ab20fcd74cc84883810e3c644c8a4c3a4f937
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-11-19 19:31:49 +0100

    Add log out

--- src/Components/Molecules/DrawerMenu/DrawerMenu.tsx
+++ src/Components/Molecules/DrawerMenu/DrawerMenu.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from "react";
 import { List, ListItem, ListItemText } from "@material-ui/core";
 import { Link as RouterLink } from "react-router-dom";
+import { fetchAuth, FetchAuthType } from "../../../authenticationSlice";
+import { useDispatch } from "react-redux";
 
 interface Props {
     closeDrawer: () => void;
@@ -8,40 +10,41 @@ interface Props {
 
 // TODO indicate active route, see https://material-ui.com/guides/composition/#link
 
-const DrawerMenu: FC<Props> = ({ closeDrawer }) => (
-    <div role="presentation" onClick={closeDrawer}>
-        <List>
-            <ListItem button component={RouterLink} to="/">
-                <ListItemText primary="Home Automation" />
-            </ListItem>
-            <ListItem button component={RouterLink} to="/dashboard">
-                <ListItemText primary="Dashboard" />
-            </ListItem>
-            <ListItem button component={RouterLink} to="/music">
-                <ListItemText primary="Music" />
-            </ListItem>
-            <ListItem button component={RouterLink} to="/gears">
-                <ListItemText primary="Gears" />
-            </ListItem>
-            <ListItem button component={RouterLink} to="/log">
-                <ListItemText primary="Logging" />
-            </ListItem>
-            <ListItem button component={RouterLink} to="/streams">
-                <ListItemText primary="Streams" />
-            </ListItem>
-            <ListItem
-                button
-                onClick={(): void => {
-                    // TODO call logout endpoint
-                    // localStorage.removeItem("hr-remember-me");
-                    // localStorage.removeItem("token");
-                    window.location.href = "/";
-                }}
-            >
-                <ListItemText primary="Log out" />
-            </ListItem>
-        </List>
-    </div>
-);
+const DrawerMenu: FC<Props> = ({ closeDrawer }) => {
+    const dispatch = useDispatch();
+    return (
+        <div role="presentation" onClick={closeDrawer}>
+            <List>
+                <ListItem button component={RouterLink} to="/">
+                    <ListItemText primary="Home Automation" />
+                </ListItem>
+                <ListItem button component={RouterLink} to="/dashboard">
+                    <ListItemText primary="Dashboard" />
+                </ListItem>
+                <ListItem button component={RouterLink} to="/music">
+                    <ListItemText primary="Music" />
+                </ListItem>
+                <ListItem button component={RouterLink} to="/gears">
+                    <ListItemText primary="Gears" />
+                </ListItem>
+                <ListItem button component={RouterLink} to="/log">
+                    <ListItemText primary="Logging" />
+                </ListItem>
+                <ListItem button component={RouterLink} to="/streams">
+                    <ListItemText primary="Streams" />
+                </ListItem>
+                <ListItem
+                    button
+                    onClick={(): void => {
+                        dispatch(fetchAuth({ type: FetchAuthType.Logout }));
+                        window.location.href = "/";
+                    }}
+                >
+                    <ListItemText primary="Log out" />
+                </ListItem>
+            </List>
+        </div>
+    );
+}
 
 export default DrawerMenu;
---
+++ src/authenticationSlice.ts
@@ -0,0 +1,84 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import ApiBaseState from "./Reducers/state.types";
+
+export interface AuthenticationState extends ApiBaseState {
+    id: number;
+    name: string;
+}
+
+export const initialState: AuthenticationState = {
+    id: 0,
+    name: "",
+    isLoading: false,
+    error: false,
+};
+
+export enum FetchAuthType {
+    Login,
+    Logout,
+    Current,
+}
+
+const authEndpoint: Record<FetchAuthType, string> = {
+    [FetchAuthType.Login]: "/auth/login",
+    [FetchAuthType.Logout]: "/auth/logout",
+    [FetchAuthType.Current]: "/api/profile/current",
+};
+
+// TODO merge FetchAuthType and authEndpoint
+// type FetchAuthType = keyof authEndpoint;
+
+export const fetchAuth = createAsyncThunk<
+    { userId: number; username: string },
+    { type: FetchAuthType, options?: any }
+>(`authentication/fetchAuth`, async ({ type, options }) => {
+    console.log(type, authEndpoint[type]);
+
+    const response = await fetch(
+        `${process.env.REACT_APP_BASE_URL}${authEndpoint[type]}`,
+        {
+            credentials: "same-origin",
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            ...options,
+        }
+    );
+
+    if (!response.ok) {
+        throw new Error(`fetchAuth ${response.statusText}`);
+    }
+    const json = await response.json();
+
+    if (json.error) {
+        throw new Error(`fetchAuth ${json.error}`);
+    }
+    return json;
+});
+
+const authenticationSlice = createSlice({
+    name: "authentication",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchAuth.pending, (state, { payload }): void => {
+            state.error = initialState.error;
+            state.isLoading = true;
+        });
+        builder.addCase(fetchAuth.fulfilled, (state, { payload }): void => {
+            state.isLoading = initialState.isLoading;
+            state.id = payload.userId;
+            state.name = payload.username;
+        });
+        builder.addCase(fetchAuth.rejected, (state, { error }): void => {
+            state.isLoading = initialState.isLoading;
+            state.id = initialState.id;
+            state.name = initialState.name;
+            state.error = error.message || "An error occurred";
+        });
+    },
+});
+
+export default authenticationSlice.reducer;
