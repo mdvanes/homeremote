5957ab434e7b3faf7b45e2e675941c2066bb78a1
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-11-22 10:42:20 +0100

    Fix unit test with mock store

--- src/Components/Molecules/DrawerMenu/DrawerMenu.test.tsx
+++ src/Components/Molecules/DrawerMenu/DrawerMenu.test.tsx
@@ -1,12 +1,36 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { fireEvent } from "@testing-library/react";
 import DrawerMenu from "./DrawerMenu";
+import authenticationReducer from "../../Providers/Authentication/authenticationSlice";
+import { renderWithProviders } from "../../../testHelpers";
+import { RootState } from "../../../Reducers";
+
+type MockRootState = Pick<RootState, "authentication">;
+
+const mockRootState: MockRootState = {
+    authentication: {
+        id: 0,
+        name: "",
+        error: false,
+        isLoading: false,
+    },
+};
+
+const mockCloseDrawer = jest.fn();
+
+const renderDrawerMenu = (initialState: MockRootState) =>
+    renderWithProviders(<DrawerMenu closeDrawer={mockCloseDrawer} />, {
+        initialState,
+        reducers: {
+            authentication: authenticationReducer,
+        },
+    });
 
 jest.mock("react-router-dom", () => ({
     Link: "mock-link",
 }));
 
-const mockCloseDrawer = jest.fn();
+const fetchSpy = jest.spyOn(window, "fetch");
 
 describe("DrawerMenu", () => {
     beforeEach(() => {
@@ -14,36 +38,35 @@ describe("DrawerMenu", () => {
     });
 
     it("has Dashboard link", () => {
-        const { getByText } = render(
-            <DrawerMenu closeDrawer={mockCloseDrawer} />
-        );
+        const { getByText } = renderDrawerMenu(mockRootState);
         expect(getByText("Dashboard")).toBeInTheDocument();
     });
 
     it("closes the drawer after a menu item is clicked", () => {
-        const { getByText } = render(
-            <DrawerMenu closeDrawer={mockCloseDrawer} />
-        );
+        const { getByText } = renderDrawerMenu(mockRootState);
         fireEvent.click(getByText("Dashboard"));
         expect(mockCloseDrawer).toHaveBeenCalledTimes(1);
     });
 
     it("logouts on logout link click", () => {
-        interface MockWindow {
-            location?: Location;
-        }
-        const oldLocation = window.location;
-        delete (window as MockWindow).location;
-        window.location = {
-            ...oldLocation,
-            href: "/foo",
-        };
-        expect(window.location.href).toBe("/foo");
-        const { getByText } = render(
-            <DrawerMenu closeDrawer={mockCloseDrawer} />
-        );
+        //     interface MockWindow {
+        //         location?: Location;
+        //     }
+        //     const oldLocation = window.location;
+        //     delete (window as MockWindow).location;
+        //     window.location = {
+        //         ...oldLocation,
+        //         href: "/foo",
+        //     };
+        //     expect(window.location.href).toBe("/foo");
+        const { getByText } = renderDrawerMenu(mockRootState);
+        fetchSpy.mockReset();
         fireEvent.click(getByText("Log out"));
         expect(mockCloseDrawer).toHaveBeenCalledTimes(1);
-        expect(window.location.href).toBe("/");
+        // expect(window.location.href).toBe("/");
+        expect(fetchSpy).toHaveBeenCalledWith(
+            "/auth/logout",
+            expect.anything()
+        );
     });
 });
