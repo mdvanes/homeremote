0edc777fa68a8b1940817ac1fc0e31990d6ca132
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-12-06 09:08:40 +0100

    Update service worker registration

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import App, { AppProps } from "./App";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+// import reportWebVitals from './reportWebVitals';
 import rootReducer from "./Reducers";
 
 const store = configureStore({
@@ -24,8 +25,8 @@ ReactDOM.render(
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register({
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register({
     onSuccess: (message) => {
         const { logSuccess } = swCallbacks;
         if (logSuccess) {
@@ -39,3 +40,8 @@ serviceWorker.register({
         }
     },
 });
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
