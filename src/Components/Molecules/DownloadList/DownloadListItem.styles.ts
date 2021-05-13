686102762d397aa0bf3cd9c99de71596dd645d7d
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2021-05-13 08:46:08 +0200

    Add some styles

--- src/Components/Molecules/DownloadList/DownloadListItem.styles.ts
+++ src/Components/Molecules/DownloadList/DownloadListItem.styles.ts
@@ -1,6 +1,14 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, lighten } from "@material-ui/core";
 
 const useStyles = makeStyles(({ palette }) => ({
+    root: {
+        "&:hover": {
+            backgroundColor: lighten(palette.primary.light, 0.8),
+        },
+    },
+    main: {
+        flexGrow: 1,
+    },
     name: {
         fontWeight: "bold",
         wordBreak: "break-word",
