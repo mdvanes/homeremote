99b4743c30f2a3f3540653558f0209b1910f7287
M.D. van Es <es@mdworld.nl>
2021-05-13 13:29:00 +0200

    Bump version

--- src/Components/Molecules/UrlToMusic/urlToMusicSlice.test.tsx
+++ src/Components/Molecules/UrlToMusic/urlToMusicSlice.test.tsx
@@ -1,3 +1,6 @@
+// Force as ES module with import. Do not use export {}, because avoid exports from unit tests.
+import "redux";
+
 /**
  * Because of isolatedModules, this does not work in UrlToMusic.test. Here, just test the value of "album".
  */
