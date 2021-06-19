229d3ee6627e7702f9d8c8f6d59c33d0d8f6b5ae
M.D. van Es <es@mdworld.nl>
2021-06-19 20:54:17 +0200

    Update dockerlist

--- src/Components/Pages/Docker/Docker.tsx
+++ src/Components/Pages/Docker/Docker.tsx
@@ -1,18 +1,13 @@
 import { FC } from "react";
-import { DockerListMod, reactDomStyleT } from "@mdworld/homeremote-dockerlist";
+import { DockerListMod } from "@mdworld/homeremote-dockerlist";
 import { useDispatch } from "react-redux";
 import { logError } from "../../Molecules/LogCard/logSlice";
-import theme from "../../../theme";
 import { Card } from "@material-ui/core";
 
 const DockerList = DockerListMod.make;
 
 const Docker: FC = () => {
     const dispatch = useDispatch();
-    const confirmButtonStyle: React.CSSProperties = {
-        backgroundColor: theme.palette.primary.light,
-        color: "white",
-    };
 
     return (
         <Card>
@@ -20,10 +15,6 @@ const Docker: FC = () => {
                 url={process.env.REACT_APP_BASE_URL || ""}
                 onError={(err: string) =>
                     dispatch(logError(`Dockerlist failure: ${err.toString()}`))
-                }
-                confirmButtonStyle={
-                    // Workaround for Rescript opaque type
-                    (confirmButtonStyle as unknown) as reactDomStyleT
                 }
             />
         </Card>
