4edbeb8f84f79243f90c538ea920dffb7247088e
M.D. van Es <es@mdworld.nl>
2022-01-01 14:47:30 +0100

    Fix RTKQ

--- src/Components/Molecules/DownloadList/DownloadList.tsx
+++ src/Components/Molecules/DownloadList/DownloadList.tsx
@@ -1,9 +1,6 @@
-import { FC, useCallback, useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { LinearProgress, List, Paper } from "@material-ui/core";
-import { DownloadListState, getDownloadList } from "./downloadListSlice";
 import { useAppDispatch } from "../../../store";
-import { RootState } from "../../../Reducers";
-import { useSelector } from "react-redux";
 import DownloadListItem from "./DownloadListItem";
 import { logError } from "../LogCard/logSlice";
 import { useGetDownloadListQuery } from "../../../Services/downloadListApi";
@@ -12,24 +9,14 @@ const UPDATE_INTERVAL_MS = 30000;
 
 const DownloadList: FC = () => {
     const dispatch = useAppDispatch();
-    // const { isLoading, downloads } = useSelector<RootState, DownloadListState>(
-    //     (state: RootState) => state.downloadList
-    // );
 
-    // TODO move to interval somehow?
-    const { data, error, isLoading } = useGetDownloadListQuery();
+    const { data, error, isLoading, isFetching } = useGetDownloadListQuery(
+        undefined,
+        {
+            pollingInterval: UPDATE_INTERVAL_MS,
+        }
+    );
     const [listItems, setListItems] = useState<JSX.Element[]>([]);
-
-    const getNewState = useCallback(async () => {
-        // const { data, error, isLoading } = useGetDownloadListQuery();
-        // const resultAction = await dispatch(getDownloadList());
-        // if (!getDownloadList.fulfilled.match(resultAction)) {
-        //     dispatch(logError("GetDownloadList failed"));
-        // }
-        // if (error) {
-        //     dispatch(logError("GetDownloadList failed"));
-        // }
-    }, [dispatch]);
 
     useEffect(() => {
         if (error) {
@@ -49,28 +36,12 @@ const DownloadList: FC = () => {
         }
     }, [dispatch, data]);
 
-    useEffect(() => {
-        getNewState();
-        // Update with long-polling for now
-        const timer = setInterval(() => {
-            getNewState();
-        }, UPDATE_INTERVAL_MS);
-        return () => {
-            clearInterval(timer);
-        };
-    }, [getNewState]);
-
-    // const listItems: JSX.Element[] = []; /*downloads
-    //     ? downloads.map<JSX.Element>((item) => (
-    //           <DownloadListItem key={item.id} item={item} />
-    //       ))
-    //     : []; */
-
-    const loadProgress = isLoading ? (
-        <LinearProgress variant="indeterminate" />
-    ) : (
-        <div style={{ height: 4 }}></div>
-    );
+    const loadProgress =
+        isLoading || isFetching ? (
+            <LinearProgress variant="indeterminate" />
+        ) : (
+            <div style={{ height: 4 }}></div>
+        );
 
     return (
         <List component={Paper}>
