2f75b4cc1af24d58b739003f4a532eb4d108d066
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2022-01-02 15:20:03 +0100

    Fix aspect ratio

--- src/Components/Molecules/DataLora/Map.styles.ts
+++ src/Components/Molecules/DataLora/Map.styles.ts
@@ -6,17 +6,13 @@ const useStyles = makeStyles(() => ({
         overflow: "clip",
     },
     card: {
-        height: 300,
-        overflow: "clip",
+        aspectRatio: "16/9",
         marginTop: 10,
-        // paddingTop: "50%",
-        // position: "relative",
+        overflow: "clip",
     },
     map: {
-        position: "relative",
-        // position: "absolute",
-        // top: 0,
         height: "100%",
+        position: "relative",
         "& .leaflet-container": {
             height: "100%",
         },
