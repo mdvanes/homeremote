ae3a150fe083dc6a490b2f8b4cb3c52944f4732b
M.D. van Es <es@mdworld.nl>
2022-01-08 15:10:01 +0100

    Fix tests

--- src/Components/Pages/Dashboard/Dashboard.test.tsx
+++ src/Components/Pages/Dashboard/Dashboard.test.tsx
@@ -6,19 +6,20 @@ jest.mock(
     "../../Molecules/SwitchBarList/SwitchBarList",
     () => "mock-switch-bar-list"
 );
-
 jest.mock("../../Molecules/LogCard/LogCard", () => "mock-log-card");
-
 jest.mock("../../Molecules/UrlToMusic/UrlToMusic", () => "mock-url-to-music");
-
 jest.mock(
     "../../Molecules/DownloadList/DownloadList",
     () => "mock-download-list"
 );
-
 jest.mock("@mdworld/homeremote-stream-player", () => "mock-stream-player");
-
 jest.mock("../../Pages/Docker/Docker", () => "mock-docker");
+jest.mock(
+    "../../Molecules/ActiveConnections/ActiveConnections",
+    () => "mock-active-connections"
+);
+jest.mock("../../Molecules/DataLora/DataLora", () => "mock-datalora");
+jest.mock("../../Molecules/VideoStream/VideoStream", () => "mock-video-stream");
 
 describe("Dashboard page", () => {
     it("contains all the control components", () => {
