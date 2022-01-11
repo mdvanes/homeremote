2cd051e98459d184aba71750d45ffa573cfe117b
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-01-11 14:29:45 +0100

    Fix polling interval

--- src/store.ts
+++ src/store.ts
@@ -4,13 +4,15 @@ import { setupListeners } from "@reduxjs/toolkit/query";
 import rootReducer from "./Reducers";
 import { downloadListApi } from "./Services/downloadListApi";
 import { activeConnectionsApi } from "./Services/activeConnectionsApi";
+import { dataloraApi } from "./Services/dataloraApi";
 
 export const store = configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(
             downloadListApi.middleware,
-            activeConnectionsApi.middleware
+            activeConnectionsApi.middleware,
+            dataloraApi.middleware
         ),
 });
 
