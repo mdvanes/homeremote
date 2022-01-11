7559b4f434bea4794bc6be615dfb7a3baf2a257c
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-01-11 19:19:04 +0100

    Fix first test

--- src/Components/Molecules/DownloadList/DownloadList.test.tsx
+++ src/Components/Molecules/DownloadList/DownloadList.test.tsx
@@ -1,10 +1,10 @@
-import { fireEvent, screen, waitFor } from "@testing-library/react";
-import React from "react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { FC } from "react";
 import { DownloadItem } from "../../../ApiTypes/downloadlist.types";
-import { RootState } from "../../../Reducers";
-import { renderWithProviders } from "../../../testHelpers";
 import DownloadList from "./DownloadList";
-import { useGetDownloadListQuery } from "../../../Services/downloadListApi";
+import { downloadListApi } from "../../../Services/downloadListApi";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 
 const fetchSpy = jest.spyOn(window, "fetch");
 
@@ -21,76 +21,93 @@ const mockDownload: DownloadItem = {
 };
 
 const createMockResponse = (mockDownload: DownloadItem): Partial<Response> => {
-    return {
-        ok: true,
-        json: () =>
-            Promise.resolve({
+    const responseBlob = new Blob(
+        [
+            JSON.stringify({
                 status: "received",
                 downloads: [mockDownload],
             }),
-    };
+        ],
+        { type: "application/json" }
+    );
+    return new Response(responseBlob, {
+        status: 200,
+    });
+};
+
+const MockStoreProvider: FC = ({ children }) => {
+    const rootReducer = combineReducers({
+        [downloadListApi.reducerPath]: downloadListApi.reducer,
+    });
+
+    const store = configureStore({
+        reducer: rootReducer,
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware().concat(downloadListApi.middleware),
+    });
+
+    return <Provider store={store}>{children}</Provider>;
 };
 
 describe("DownloadList", () => {
-    it("dummy", () => {
-        expect(true).toBe(true);
+    beforeEach(() => {
+        const mockResponse = createMockResponse(mockDownload);
+        fetchSpy.mockResolvedValue(mockResponse as Response);
+        jest.useFakeTimers();
     });
 
-    // type MockRootState = Pick<RootState, "downloadList">;
-    // const renderDownloadList = (initialState: MockRootState) =>
-    //     renderWithProviders(<DownloadList />, {
-    //         initialState,
-    //         reducers: { downloadList: downloadListReducer },
-    //     });
-    // beforeEach(() => {
-    //     const mockResponse = createMockResponse(mockDownload);
-    //     fetchSpy.mockResolvedValue(mockResponse as Response);
-    //     jest.useFakeTimers();
-    // });
-    // it("renders download info", async () => {
-    //     renderDownloadList({
-    //         downloadList: initialState,
-    //     });
-    //     expect(screen.queryByText("SomeName")).not.toBeInTheDocument();
-    //     await waitFor(() => {
-    //         expect(screen.getByText("SomeName")).toBeInTheDocument();
-    //     });
-    //     // State, download speed, upload speed
-    //     expect(
-    //         screen.getByText("SomeState ▼200 kB/s ▲100 kB/s")
-    //     ).toBeInTheDocument();
-    //     // Size, percentage, ETA
-    //     expect(screen.getByText(/4GB/)).toBeInTheDocument();
-    //     expect(screen.getByText(/50%/)).toBeInTheDocument();
-    //     expect(screen.getByText(/100H remaining/)).toBeInTheDocument();
-    // });
-    // it("can toggle download", async () => {
-    //     renderDownloadList({
-    //         downloadList: initialState,
-    //     });
-    //     await waitFor(() => {
-    //         expect(screen.getByText("SomeName")).toBeInTheDocument();
-    //     });
-    //     expect(screen.queryByText("SomePausedState")).not.toBeInTheDocument();
-    //     const toggleButton = screen.getByRole("button");
-    //     const mockResponse = createMockResponse({
-    //         ...mockDownload,
-    //         state: "SomePausedState",
-    //         simpleState: "paused",
-    //     });
-    //     fetchSpy.mockReset();
-    //     // Fetch for pauseDownload and getDownloadList (on interval)
-    //     fetchSpy.mockResolvedValue(mockResponse as Response);
-    //     expect(fetchSpy).not.toBeCalled();
-    //     fireEvent.click(toggleButton);
-    //     await waitFor(() => {
-    //         expect(screen.getByText("SomePausedState")).toBeInTheDocument();
-    //     });
-    //     expect(screen.queryByText("SomeState")).not.toBeInTheDocument();
-    //     expect(fetchSpy).toBeCalledTimes(2);
-    //     expect(fetchSpy).toBeCalledWith(
-    //         "/api/downloadlist/pauseDownload/14",
-    //         expect.objectContaining({})
-    //     );
-    // });
+    it("renders download info", async () => {
+        render(
+            <MockStoreProvider>
+                <DownloadList />
+            </MockStoreProvider>
+        );
+
+        expect(screen.queryByText("SomeName")).not.toBeInTheDocument();
+        await waitFor(() => {
+            expect(screen.getByText("SomeName")).toBeInTheDocument();
+        });
+        // State, download speed, upload speed
+        expect(
+            screen.getByText("SomeState ▼200 kB/s ▲100 kB/s")
+        ).toBeInTheDocument();
+        // Size, percentage, ETA
+        expect(screen.getByText(/4GB/)).toBeInTheDocument();
+        expect(screen.getByText(/50%/)).toBeInTheDocument();
+        expect(screen.getByText(/100H remaining/)).toBeInTheDocument();
+    });
+
+    it("can toggle download", async () => {
+        render(
+            <MockStoreProvider>
+                <DownloadList />
+            </MockStoreProvider>
+        );
+
+        await waitFor(() => {
+            expect(screen.getByText("SomeName")).toBeInTheDocument();
+        });
+        expect(screen.queryByText("SomePausedState")).not.toBeInTheDocument();
+        const toggleButton = screen.getByRole("button");
+
+        const mockResponse = createMockResponse({
+            ...mockDownload,
+            state: "SomePausedState",
+            simpleState: "paused",
+        });
+        fetchSpy.mockReset();
+        // Fetch for pauseDownload and getDownloadList (on interval)
+        fetchSpy.mockResolvedValue(mockResponse as Response);
+        expect(fetchSpy).not.toBeCalled();
+        fireEvent.click(toggleButton);
+        // await waitFor(() => {
+        //     expect(screen.getByText("SomePausedState")).toBeInTheDocument();
+        // });
+        // expect(screen.queryByText("SomeState")).not.toBeInTheDocument();
+        expect(fetchSpy).toBeCalledTimes(2);
+        expect(fetchSpy).toBeCalledWith(
+            "/api/downloadlist/pauseDownload/14",
+            expect.objectContaining({})
+        );
+    });
 });
