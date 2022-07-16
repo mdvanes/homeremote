888f5b96468d6a9b549bbf6a1b07bfdd175bbf2c
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-07-16 20:29:50 +0200

    Add nowplaying endpoints

--- apps/server/src/nowplaying/nowplaying.controller.ts
+++ apps/server/src/nowplaying/nowplaying.controller.ts
@@ -5,6 +5,8 @@ import {
     HttpException,
     HttpStatus,
     UseGuards,
+    Headers,
+    Req,
 } from "@nestjs/common";
 import {
     getNowPlaying,
@@ -13,18 +15,61 @@ import {
 } from "@mdworld/homeremote-stream-player-server";
 import { JwtAuthGuard } from "../auth/jwt-auth.guard";
 import got from "got";
+import { ConfigService } from "@nestjs/config";
+import { LocalAuthGuard } from "../auth/local-auth.guard";
+import { LoginRequest } from "../login/LoginRequest.types";
 
 @Controller("api/nowplaying")
 export class NowplayingController {
     private readonly logger: Logger;
 
-    constructor() {
+    constructor(private configService: ConfigService) {
         this.logger = new Logger(NowplayingController.name);
     }
 
     @UseGuards(JwtAuthGuard)
     @Get("radio2")
     async getRadio2(): Promise<NowPlayingResponse | undefined> {
+        try {
+            const response = await getNowPlaying(ChannelName.RADIO2);
+            return response;
+        } catch (error) {
+            this.logger.error(error);
+            throw new HttpException(
+                error as Error,
+                HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    // Consider using JWT as API instead of random string: https://jwt.io/
+    // Do not use use @UseGuards(LocalAuthGuard) like in login.controller. Keep separate authentication with a separate app as a kind of Role Based Access Control
+    // Test with `curl -X GET -H "token: 123" http://localhost:4200/api/nowplaying/stereo8/radio2`
+    @Get("stereo8/radio2")
+    async getStereo8Radio2(
+        @Headers() headers: { token?: string }
+    ): Promise<NowPlayingResponse | undefined> {
+        const notFound = {
+            statusCode: HttpStatus.NOT_FOUND,
+            message: "Cannot GET /api/nowplaying/stereo8/radio2",
+            error: "Not Found",
+        };
+        const stereo8Token = this.configService.get<string | undefined>(
+            "STEREO8_TOKEN"
+        );
+        if (!stereo8Token) {
+            this.logger.error(
+                "/stereo8/radio2 called, but token not configured"
+            );
+            // Emulate an error as if the endpoint does not exist
+            throw new HttpException(notFound, HttpStatus.NOT_FOUND);
+        } else if (headers.token !== stereo8Token) {
+            this.logger.error("/stereo8/radio2 called with invalid token");
+            // Emulate an error as if the endpoint does not exist
+            throw new HttpException(notFound, HttpStatus.NOT_FOUND);
+        }
+
+        this.logger.log("/stereo8/radio2 called with valid token");
         try {
             const response = await getNowPlaying(ChannelName.RADIO2);
             return response;
@@ -64,6 +109,36 @@ export class NowplayingController {
     async getRadio3(): Promise<NowPlayingResponse | undefined> {
         try {
             const response = await getNowPlaying(ChannelName.RADIO3);
+            return response;
+        } catch (error) {
+            this.logger.error(error);
+            throw new HttpException(
+                error as Error,
+                HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get("sky")
+    async getSky(): Promise<NowPlayingResponse | undefined> {
+        try {
+            const response = await getNowPlaying(ChannelName.SKY);
+            return response;
+        } catch (error) {
+            this.logger.error(error);
+            throw new HttpException(
+                error as Error,
+                HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get("pinguin")
+    async getPinguin(): Promise<NowPlayingResponse | undefined> {
+        try {
+            const response = await getNowPlaying(ChannelName.PINGUIN);
             return response;
         } catch (error) {
             this.logger.error(error);
