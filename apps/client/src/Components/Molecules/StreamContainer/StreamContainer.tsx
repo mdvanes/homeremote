032efcd8b3a4714bddb782040c30c8a2966d99b5
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-10-14 08:06:49 +0200

    add ports

--- apps/client/src/Components/Molecules/StreamContainer/StreamContainer.tsx
+++ apps/client/src/Components/Molecules/StreamContainer/StreamContainer.tsx
@@ -1,10 +1,15 @@
-import HomeremoteStreamPlayer from "@mdworld/homeremote-stream-player";
+import HomeremoteStreamPlayer, {
+    Ports,
+} from "@mdworld/homeremote-stream-player";
 import { Box, Card, CardContent } from "@mui/material";
 import { FC, useCallback, useEffect, useState } from "react";
+import Jukebox from "../Jukebox/Jukebox";
 import StyledStreamPlayer from "./StyledStreamPlayer";
 
 const StreamContainer: FC = () => {
     const [play, setPlay] = useState(false);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [ports, setPorts] = useState<Ports | null>(null);
 
     // handle what happens on key press
     const handleKeyPress = useCallback((event) => {
@@ -18,8 +23,14 @@ const StreamContainer: FC = () => {
         if (event.key === "t") {
             console.log("toggle radio v stream");
             setPlay((prev) => !prev);
+
+            if (ports?.receivePlayPauseStatusPort?.send) {
+                ports.receivePlayPauseStatusPort.send(
+                    isPlaying ? "Pause" : "Play"
+                );
+            }
         }
-    }, []);
+    }, [ports, isPlaying]);
 
     useEffect(() => {
         document.addEventListener("keydown", handleKeyPress);
@@ -29,15 +40,38 @@ const StreamContainer: FC = () => {
         };
     }, [handleKeyPress]);
 
+    useEffect(() => {
+        const subscribe = ports?.setPlayPauseStatusPort?.subscribe;
+        if (subscribe) {
+            subscribe((newStatus) => {
+                setIsPlaying(newStatus === "Play" ? true : false);
+            });
+        }
+    }, [ports]);
+
     return (
         <>
             <StyledStreamPlayer>
-                <HomeremoteStreamPlayer url={process.env.NX_BASE_URL || ""} />
+                <HomeremoteStreamPlayer
+                    url={process.env.NX_BASE_URL || ""}
+                    
+                />
             </StyledStreamPlayer>
             <Box marginTop={2} />
-            <Card sx={{ height: 250 }}>
-                <CardContent>{play ? "playing" : "stopped"}</CardContent>
-            </Card>
+            <button
+                style={{ marginTop: "1rem" }}
+                onClick={() => {
+                    console.log(ports, ports?.receivePlayPauseStatusPort);
+                    if (ports?.receivePlayPauseStatusPort?.send) {
+                        ports.receivePlayPauseStatusPort.send(
+                            isPlaying ? "Pause" : "Play"
+                        );
+                    }
+                }}
+            >
+                External toggle: {isPlaying ? "playing" : "paused"}
+            </button>
+            <Jukebox play={play} />
         </>
     );
 };
