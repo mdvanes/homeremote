4702820e4fb44753ca1c33df3115d88283420548
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-10-25 14:01:50 +0200

    clean up test

--- apps/client/src/Components/Molecules/UrlToMusic/UrlToMusic.test.tsx
+++ apps/client/src/Components/Molecules/UrlToMusic/UrlToMusic.test.tsx
@@ -93,7 +93,7 @@ describe("UrlToMusic", () => {
         const { urlInput, getInfoButton, rendered } = await expectSetUrl();
 
         fetchMock.mockResponse(JSON.stringify(mockGetInfoResponse));
-        await userEvent.click(getInfoButton);
+        userEvent.click(getInfoButton);
 
         const titleInput = await screen.findByRole("textbox", {
             name: "Title",
@@ -118,8 +118,9 @@ describe("UrlToMusic", () => {
         const { getInfoButton } = await expectSetUrl();
 
         fetchMock.mockReject(Error("some getinfo error"));
-        await userEvent.click(getInfoButton);
+        userEvent.click(getInfoButton);
 
+        await screen.findByText("[FETCH_ERROR] Error: some getinfo error");
         expect(
             screen.getByText("[FETCH_ERROR] Error: some getinfo error")
         ).toBeVisible();
@@ -129,7 +130,7 @@ describe("UrlToMusic", () => {
         const { getInfoButton } = await expectSetUrl();
 
         fetchMock.mockResponse(JSON.stringify(mockGetInfoResponse));
-        await userEvent.click(getInfoButton);
+        userEvent.click(getInfoButton);
 
         const titleInput = await screen.findByRole("textbox", {
             name: "Title",
@@ -151,7 +152,7 @@ describe("UrlToMusic", () => {
             JSON.stringify(mockGetMusicResponse),
             JSON.stringify(mockGetMusicProgressResponse)
         );
-        await userEvent.click(getMusicButton);
+        userEvent.click(getMusicButton);
 
         const resultText = await screen.findByText(
             /Result in some\/result\/path\.txt/
