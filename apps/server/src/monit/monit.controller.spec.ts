1c1b0a4f0ebdc78b9f087394ea758b8db40b2114
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-10-26 10:08:35 +0200

    fix test

--- apps/server/src/monit/monit.controller.spec.ts
+++ apps/server/src/monit/monit.controller.spec.ts
@@ -1,15 +1,29 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { MonitController } from "./monit.controller";
+import got, { Response, CancelableRequest } from "got";
+import { ConfigService } from "@nestjs/config";
+
+jest.mock("got");
+// const mockGot = mocked(got);
 
 describe("MonitController", () => {
     let controller: MonitController;
+    let configService: ConfigService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [MonitController],
+            providers: [
+                { provide: ConfigService, useValue: { get: jest.fn() } },
+            ],
         }).compile();
 
+        configService = module.get<ConfigService>(ConfigService);
         controller = module.get<MonitController>(MonitController);
+
+        jest.spyOn(configService, "get").mockImplementation((envName) => {
+            return "a,b,c;d,e,f";
+        });
     });
 
     it("should be defined", () => {
