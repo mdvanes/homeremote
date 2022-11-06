3ba6b256f80447b234cd9465a857da07d37465fa
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-11-06 13:11:22 +0100

    restyle

--- apps/client/src/Components/Molecules/DockerList/DockerList.tsx
+++ apps/client/src/Components/Molecules/DockerList/DockerList.tsx
@@ -49,33 +49,34 @@ const DockerList: FC<DockerListProps> = ({ onError }) => {
     const notRunningContainers = (data.containers ?? []).filter(
         (c) => c.State !== "running"
     );
-    const containers = isOpen ? allContainers : notRunningContainers;
-    const containers1 = containers.slice(0, containers.length / 2);
-    const containers2 = containers.slice(containers.length / 2);
-
     const containerDots = runningContainers.map((c) => (
-        <ContainerDot info={c} />
+        <ContainerDot key={c.Id} info={c} />
     ));
+    const containers = isOpen ? allContainers : notRunningContainers;
+    const containers1 = containers.slice(0, Math.ceil(containers.length / 2));
+    const containers2 = containers.slice(Math.ceil(containers.length / 2));
 
     return (
         <>
             <LoadingDot isLoading={isLoading || isFetching} noMargin />
-            <Grid container gap={0.5}>
-                <Grid item xs>
-                    <Stack spacing={0.5}>{containers1.map(mapInfo)}</Stack>
-                </Grid>
-                <Grid item xs>
-                    <Stack spacing={0.5}>{containers2.map(mapInfo)}</Stack>
-                </Grid>
-            </Grid>
             <>
                 {!isOpen && containers.length === 0 && (
                     <Box sx={{ marginRight: 1, display: "inline-block" }}>
                         No stopped containers
                     </Box>
                 )}
-                {!isOpen && <>{containerDots}</>}
             </>
+            <Grid container gap={0.5}>
+                <Grid item xs>
+                    <Stack spacing={0.5}>{containers1.map(mapInfo)}</Stack>
+                </Grid>
+                <Grid item xs>
+                    <Stack spacing={0.5}>
+                        {containers2.map(mapInfo)}
+                        {!isOpen && <div>{containerDots}</div>}
+                    </Stack>
+                </Grid>
+            </Grid>
             <CardExpandBar
                 isOpen={isOpen}
                 setIsOpen={setIsOpen}
