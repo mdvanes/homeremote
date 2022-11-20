df9f29dd33391585e245f9cc61fc50493806b891
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-11-20 10:25:16 +0100

    add types

--- libs/types/src/lib/nextup.types.ts
+++ libs/types/src/lib/nextup.types.ts
@@ -3,6 +3,38 @@ export interface NextUpVideoItem {
     SeriesName: string;
 }
 
+export interface UserLatestItem {
+    Name: string;
+    SeriesName: string;
+    CommunityRating: number;
+    ParentIndexNumber: number;
+    IndexNumber: number;
+    Id: string;
+    SeriesId: string;
+    ImageTags: {
+        Primary: string;
+    };
+}
+
+export type UserLatestResponse = UserLatestItem[];
+
+export interface ShowNextUpItem {
+    Id: string;
+    SeriesName: string;
+    ParentIndexNumber: number;
+    IndexNumber: number;
+    Name: string;
+    ProductionYear: number;
+    CommunityRating: number;
+    ImageTags: {
+        Primary: string;
+    };
+}
+
+export interface ShowNextUpResponse {
+    Items: [ShowNextUpItem];
+}
+
 export interface GetNextUpResponse {
-    items: NextUpVideoItem[];
+    items: ShowNextUpItem[];
 }
