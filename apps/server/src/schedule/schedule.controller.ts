5c64264ac4614023a49e6948f58ffd41e8f5a7b9
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-11-22 21:09:50 +0100

    add thumbnail to schedule

--- apps/server/src/schedule/schedule.controller.ts
+++ apps/server/src/schedule/schedule.controller.ts
@@ -5,7 +5,9 @@ import {
     HttpException,
     HttpStatus,
     Logger,
+    Param,
     Request,
+    StreamableFile,
     UseGuards,
 } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
@@ -16,9 +18,14 @@ import { AuthenticatedRequest } from "../login/LoginRequest.types";
 @Controller("api/schedule")
 export class ScheduleController {
     private readonly logger: Logger;
+    private readonly SCHEDULE_URL: string;
 
     constructor(private configService: ConfigService) {
         this.logger = new Logger(ScheduleController.name);
+
+        // Get the API from host:port/apibuilder/
+        this.SCHEDULE_URL =
+            this.configService.get<string>("SCHEDULE_URL") || "";
     }
 
     @UseGuards(JwtAuthGuard)
@@ -28,14 +35,34 @@ export class ScheduleController {
     ): Promise<GetScheduleResponse> {
         this.logger.verbose(`[${req.user.name}] GET to /api/schedule`);
 
-        // Get the API from host:port/apibuilder/
-        const SCHEDULE_URL =
-            this.configService.get<string>("SCHEDULE_URL") || "";
-
         try {
-            const url = `${SCHEDULE_URL}/?cmd=future`;
+            const url = `${this.SCHEDULE_URL}/?cmd=future`;
             const response: GetScheduleResponse = await got(url).json();
             return response;
+        } catch (err) {
+            this.logger.error(`[${req.user.name}] ${err}`);
+            throw new HttpException(
+                "failed to receive downstream data",
+                HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get("thumbnail/:id")
+    async getThumbnail(
+        @Param("id") id: string,
+        @Request() req: AuthenticatedRequest
+    ): Promise<StreamableFile> {
+        this.logger.verbose(
+            `[${req.user.name}] GET to /api/schedule/thumbnail/:id ${id}`
+        );
+
+        try {
+            const streamUrl = `${this.SCHEDULE_URL}/?cmd=show.getbanner&indexerid=${id}&media_format=thumb`;
+
+            const str = got.stream(streamUrl);
+            return new StreamableFile(str);
         } catch (err) {
             this.logger.error(`[${req.user.name}] ${err}`);
             throw new HttpException(
