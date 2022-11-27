923ad2834e30c9a2d62d45fbb0a8fb4af3829a31
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-11-27 11:17:33 +0100

    refactor

--- apps/server/src/energyusage/energyusage.controller.ts
+++ apps/server/src/energyusage/energyusage.controller.ts
@@ -38,6 +38,22 @@ const strToConfigs = (sensorConfig: string): SensorConfig[] => {
     return sensors;
 };
 
+// const temperatureResponseToEntry = (response, temperatureIndex) => {
+//     const sensor = temperatureSensors[temperatureIndex];
+//     const temperatureEntry = response.result[index];
+//     if (temperatureEntry.d !== temperatureEntry.d) {
+//         throw new Error("days do not match");
+//     }
+//     return [
+//         sensor.name,
+//         {
+//             avg: temperatureEntry.ta,
+//             high: temperatureEntry.te,
+//             low: temperatureEntry.tm,
+//         },
+//     ];
+// };
+
 @Controller("api/energyusage")
 export class EnergyUsageController {
     private readonly logger: Logger;
@@ -51,7 +67,6 @@ export class EnergyUsageController {
         const baseUrl = this.configService.get<string>("DOMOTICZ_URI") || "";
         const DOMOTICZ_SENSORS =
             this.configService.get<string>("DOMOTICZ_SENSORS") || "";
-        // console.log(DOMOTICZ_SENSORS);
         this.apiConfig = {
             baseUrl,
             sensors: strToConfigs(DOMOTICZ_SENSORS),
@@ -70,72 +85,52 @@ export class EnergyUsageController {
         this.logger.verbose(`[${req.user.name}] GET to /api/energyusage/gas`);
 
         try {
-            // const domoticzuri =
-            //     this.configService.get<string>("DOMOTICZ_URI") || "";
-            const domoticzuri = this.apiConfig.baseUrl;
-            console.log(this.apiConfig);
-
             // NOTE: it's assumed that the first sensor is the baseline, the gas counter
-
             const [gasSensor, ...temperatureSensors] = this.apiConfig.sensors;
 
             const gasCounterResponse: GotGasUsageResponse = await got(
                 this.getAPI(gasSensor)
             ).json();
-            // const gasPromise = temperaturePromises;
 
             const temperaturePromises = temperatureSensors.map(
                 (sensor): Promise<GotTempResponse> => {
                     return got(this.getAPI(sensor)).json();
                 }
             );
-            // console.log(temperaturePromises);
             const temperatureResponses = await Promise.all<GotTempResponse>(
                 temperaturePromises
             );
 
-            /*
-            tempInside1Response
-            result[] -> {d (day), hu (humidity) string, (numbers:) ta (avg), te (high), tm (low)}
-            resultPrev
-            */
             const aggregated: EnergyUsageGetGasUsageResponse = {
                 ...gasCounterResponse,
-                result: gasCounterResponse.result.map((entry, index) => {
-                    // const tempInside1Entry = tempInside1Response.result[index];
-                    const tempInside1Entry =
-                        temperatureResponses[0].result[index];
-                    if (tempInside1Entry.d !== entry.d) {
-                        throw new Error("days do not match");
-                    }
-                    // const tempOutside1Entry =
-                    //     tempOutside1Response.result[index];
-                    const tempOutside1Entry =
-                        temperatureResponses[1].result[index];
-                    if (tempOutside1Entry.d !== entry.d) {
-                        throw new Error("days do not match");
-                    }
+                result: gasCounterResponse.result.map((gasEntry, index) => {
+                    const temperatureEntries = temperatureResponses.map(
+                        (response, temperatureIndex) => {
+                            const sensor = temperatureSensors[temperatureIndex];
+                            const temperatureEntry = response.result[index];
+                            if (temperatureEntry.d !== temperatureEntry.d) {
+                                throw new Error("days do not match");
+                            }
+                            return [
+                                sensor.name,
+                                {
+                                    avg: temperatureEntry.ta,
+                                    high: temperatureEntry.te,
+                                    low: temperatureEntry.tm,
+                                },
+                            ];
+                        }
+                    );
+
                     const result: EnergyUsageGasItem = {
-                        counter: entry.c,
-                        used: entry.v,
-                        day: entry.d,
-                        temp: {
-                            tempInside1: {
-                                avg: tempInside1Entry.ta,
-                                high: tempInside1Entry.te,
-                                low: tempInside1Entry.tm,
-                            },
-                            tempOutside1: {
-                                avg: tempOutside1Entry.ta,
-                                high: tempOutside1Entry.te,
-                                low: tempOutside1Entry.tm,
-                            },
-                        },
+                        counter: gasEntry.c,
+                        used: gasEntry.v,
+                        day: gasEntry.d,
+                        temp: Object.fromEntries(temperatureEntries),
                     };
                     return result;
                 }),
             };
-            // console.log(aggregated);
 
             return aggregated;
         } catch (err) {
