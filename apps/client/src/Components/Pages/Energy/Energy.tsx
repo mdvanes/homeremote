d9a184749a0f4ba317c75a73f57c1020085b4497
mdvanes <4253562+mdvanes@users.noreply.github.com>
2024-05-05 14:42:58 +0200

    chore: add energy tabs

--- apps/client/src/Components/Pages/Energy/Energy.tsx
+++ apps/client/src/Components/Pages/Energy/Energy.tsx
@@ -1,51 +1,62 @@
-// temperatures / humidity
-// gas
-// electra
-// water?
-
-import {
-    useGetTemperatureQuery,
-    useGetWaterQuery,
-} from "../../../Services/energyUsageApi";
-import { FC } from "react";
+import { useGetWaterQuery } from "../../../Services/energyUsageApi";
+import { FC, useState } from "react";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import { ChartClimate } from "./ChartClimate";
 
 export const Energy: FC = () => {
-    const { data: temperatureData } = useGetTemperatureQuery(undefined);
     const { data: waterData } = useGetWaterQuery(undefined);
 
+    const [value, setValue] = useState("1");
+
+    const handleTabChange = (
+        _event: React.SyntheticEvent,
+        newValue: string
+    ) => {
+        setValue(newValue);
+    };
+
     return (
-        <>
-            <div>
-                temperature:
-                {temperatureData &&
-                    temperatureData.length &&
-                    temperatureData.map((sensors) => (
-                        <ul>
-                            {sensors.map((entry) => (
-                                <li>
-                                    {entry.last_changed} {entry.entity_id}{" "}
-                                    {entry.state}
-                                </li>
+        <Box sx={{ width: "100%", typography: "body1" }}>
+            <TabContext value={value}>
+                <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                    <TabList
+                        onChange={handleTabChange}
+                        aria-label="lab API tabs example"
+                    >
+                        <Tab label="Climate" value="1" />
+                        <Tab label="Gas" value="2" />
+                        <Tab label="Electric" value="3" />
+                        <Tab label="Water" value="4" />
+                    </TabList>
+                </Box>
+                <TabPanel value="1">
+                    <ChartClimate />
+                </TabPanel>
+                <TabPanel value="2">Item Two</TabPanel>
+                <TabPanel value="3">Item Three</TabPanel>
+                <TabPanel value="4">
+                    <div>
+                        water:
+                        {waterData &&
+                            waterData.length &&
+                            waterData.map((sensors) => (
+                                <ul>
+                                    {sensors.map((entry) => (
+                                        <li>
+                                            {entry.last_changed}{" "}
+                                            {entry.entity_id} {entry.state}
+                                        </li>
+                                    ))}
+                                </ul>
                             ))}
-                        </ul>
-                    ))}
-            </div>
-            <div>
-                water:
-                {waterData &&
-                    waterData.length &&
-                    waterData.map((sensors) => (
-                        <ul>
-                            {sensors.map((entry) => (
-                                <li>
-                                    {entry.last_changed} {entry.entity_id}{" "}
-                                    {entry.state}
-                                </li>
-                            ))}
-                        </ul>
-                    ))}
-            </div>
-        </>
+                    </div>
+                </TabPanel>
+            </TabContext>
+        </Box>
     );
 };
 
