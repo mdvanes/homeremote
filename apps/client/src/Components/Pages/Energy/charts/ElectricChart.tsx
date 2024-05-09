6cbc2df874ccdc96b66423e2bf0c8a70882f1346
mdvanes <4253562+mdvanes@users.noreply.github.com>
2024-05-09 18:45:49 +0200

    refactor

--- apps/client/src/Components/Pages/Energy/charts/ElectricChart.tsx
+++ apps/client/src/Components/Pages/Energy/charts/ElectricChart.tsx
@@ -1,9 +1,49 @@
-import { Box, LinearProgress } from "@mui/material";
-import { FC } from "react";
-import { useGetElectricExportsQuery } from "../../../../Services/generated/energyUsageApi";
+import { Box, LinearProgress, Stack } from "@mui/material";
+import { FC, useState } from "react";
+import {
+    GetElectricExportsApiResponse,
+    useGetElectricExportsQuery,
+} from "../../../../Services/generated/energyUsageApi";
 import styles from "./ElectricChart.module.scss";
 
+const ElectricChartRow: FC<{ data: GetElectricExportsApiResponse[0] }> = ({
+    data,
+}) => {
+    const areSomeEmpty = data.entries?.some((e) => typeof e.v === "undefined");
+
+    if (!data.dateMillis) {
+        return null;
+    }
+
+    return (
+        <tr key={data.exportName}>
+            <td>
+                <input type="checkbox" defaultChecked={!areSomeEmpty} />
+            </td>
+            <td>{data.exportName}</td>
+            <td className={`${areSomeEmpty ? styles["empty"] : ""}`}>
+                {new Date(data.dateMillis).toLocaleDateString("en-gb", {
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit",
+                    weekday: "short",
+                })}
+            </td>
+            {data.entries?.map((e, i) => (
+                <td
+                    key={i}
+                    className={typeof e.v === "undefined" ? "empty" : ""}
+                    title={e.time}
+                >
+                    {e.v}
+                </td>
+            ))}
+        </tr>
+    );
+};
+
 export const ElectricChart: FC = () => {
+    const [filterMonth, setFilterMonth] = useState<string | undefined>();
     const { data, isLoading, isFetching, error } = useGetElectricExportsQuery();
 
     if (error) {
@@ -14,72 +54,64 @@ export const ElectricChart: FC = () => {
         return <LinearProgress />;
     }
 
+    const isSelectedRange = (file: GetElectricExportsApiResponse[0]) => {
+        if (!file.dateMillis) {
+            return false;
+        }
+        const monthIndex = new Date(file.dateMillis).getMonth();
+
+        if (filterMonth === "april" && monthIndex !== 3) {
+            return false;
+        }
+
+        if (filterMonth === "may" && monthIndex !== 4) {
+            return false;
+        }
+
+        return true;
+        // console.log(
+        //     new Date(file.dateMillis).getMonth()
+        // );
+        // return file.date;
+    };
+
     return (
-        <Box className={styles["electric-chart-table"]}>
-            <table>
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>file name</th>
-                        <th>date</th>
-                        {data?.[0] &&
-                            data?.[0].entries?.map((e) => (
-                                <th>{e.time?.slice(-5)}</th>
-                            ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {data?.map((file) => {
-                        const areSomeEmpty = file.entries?.some(
-                            (e) => typeof e.v === "undefined"
-                        );
-                        return (
-                            <>
-                                {file.dateMillis && (
-                                    <tr key={file.exportName}>
-                                        <td>
-                                            <input
-                                                type="checkbox"
-                                                checked={!areSomeEmpty}
-                                            />
-                                        </td>
-                                        <td>{file.exportName}</td>
-                                        <td
-                                            className={`${
-                                                areSomeEmpty
-                                                    ? styles["empty"]
-                                                    : ""
-                                            }`}
-                                        >
-                                            {new Date(
-                                                file.dateMillis
-                                            ).toLocaleDateString("en-gb", {
-                                                year: "numeric",
-                                                month: "2-digit",
-                                                day: "2-digit",
-                                                weekday: "short",
-                                            })}
-                                        </td>
-                                        {file.entries?.map((e, i) => (
-                                            <td
-                                                key={i}
-                                                className={
-                                                    typeof e.v === "undefined"
-                                                        ? "empty"
-                                                        : ""
-                                                }
-                                                title={e.time}
-                                            >
-                                                {e.v}
-                                            </td>
-                                        ))}
-                                    </tr>
-                                )}
-                            </>
-                        );
-                    })}{" "}
-                </tbody>
-            </table>
-        </Box>
+        <>
+            <Stack direction="row" spacing={1} marginBottom={1}>
+                <select
+                    className={styles["select"]}
+                    name="month"
+                    id=""
+                    onChange={(v) => {
+                        setFilterMonth(v.target.value);
+                    }}
+                >
+                    <option value=""></option>
+                    <option value="april">april</option>
+                    <option value="may">may</option>
+                </select>
+                <div>{filterMonth}</div>
+            </Stack>
+            <Box className={styles["electric-chart-table"]}>
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>file name</th>
+                            <th>date</th>
+                            {data?.[0] &&
+                                data?.[0].entries?.map((e) => (
+                                    <th key={e.time}>{e.time?.slice(-5)}</th>
+                                ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data?.filter(isSelectedRange).map((file) => (
+                            <ElectricChartRow data={file} />
+                        ))}
+                    </tbody>
+                </table>
+            </Box>
+        </>
     );
 };
