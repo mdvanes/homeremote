97a3008e2b81c520f2a92d045dddf2e85da141f5
mdvanes <4253562+mdvanes@users.noreply.github.com>
2024-09-16 09:35:20 +0200

    add simple speedtesttracker

---
+++ apps/client/src/Components/Molecules/SpeedTestCard/SpeedTestCard.tsx
@@ -0,0 +1,112 @@
+import SpeedIcon from "@mui/icons-material/Speed";
+import {
+    Card,
+    CardContent,
+    IconButton,
+    Stack,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+} from "@mui/material";
+import { FC, useEffect, useState } from "react";
+
+interface SpeedTestResponse {
+    message: string;
+    data: {
+        id: number;
+        ping: number;
+        download: number;
+        upload: number;
+        server_id: number;
+        server_host: string;
+        server_name: string;
+        url: string;
+        scheduled: boolean;
+        failed: boolean;
+        created_at: string;
+        updated_at: string;
+    };
+}
+
+const FORMAT_DEFAULT_LOCALE = "nl-NL";
+
+const FORMAT_DATE = new Intl.DateTimeFormat(FORMAT_DEFAULT_LOCALE, {
+    hour: "2-digit",
+    minute: "2-digit",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+});
+
+const BASE_URL = "http://192.168.0.8:8089";
+
+// NOTE: this hacky implementation is because the API is deprecated, but there is no replacement API yet.
+const fetchSpeedTest = async () => {
+    const response = await fetch(`${BASE_URL}/api/speedtest/latest`);
+    const data: SpeedTestResponse = await response.json();
+    return data;
+};
+
+export const SpeedTestCard: FC = () => {
+    const [speedTestResult, setSpeedTestResult] = useState<SpeedTestResponse>();
+
+    const fetchSpeedTestAndSet = async () => {
+        const result = await fetchSpeedTest();
+        setSpeedTestResult(result);
+    };
+
+    useEffect(() => {
+        fetchSpeedTestAndSet();
+    }, []);
+
+    if (!speedTestResult) {
+        return null;
+    }
+
+    return (
+        <Card>
+            <CardContent>
+                <Stack direction="row" alignItems="center" gap={4}>
+                    <IconButton onClick={fetchSpeedTestAndSet}>
+                        <SpeedIcon />
+                    </IconButton>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>down</TableCell>
+                                <TableCell>up</TableCell>
+                                <TableCell>ping</TableCell>
+                                <TableCell>timestamp</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow
+                                hover
+                                onClick={() => window.open(BASE_URL)}
+                            >
+                                <TableCell>
+                                    {speedTestResult.data.download}
+                                </TableCell>
+                                <TableCell>
+                                    {speedTestResult.data.upload}
+                                </TableCell>
+                                <TableCell>
+                                    {speedTestResult.data.ping}
+                                </TableCell>
+                                <TableCell>
+                                    {FORMAT_DATE.format(
+                                        new Date(
+                                            speedTestResult.data.updated_at
+                                        )
+                                    )}
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </Stack>
+            </CardContent>
+        </Card>
+    );
+};
