23e1f71e6f9901624ed3d29aee30169915b7a97c
mdvanes <4253562+mdvanes@users.noreply.github.com>
2025-04-28 15:44:53 +0200

    fix: fix blank tracker map

--- apps/client/src/Components/Molecules/DataLora/useLocQuery.ts
+++ apps/client/src/Components/Molecules/DataLora/useLocQuery.ts
@@ -1,6 +1,9 @@
 import { TrackerItem, TrackerQueryType } from "@homeremote/types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useGetCoordsQuery } from "../../../Services/dataloraApi";
+import { getErrorMessage } from "../../../Utils/getErrorMessage";
+import { useAppDispatch } from "../../../store";
+import { logInfo } from "../LogCard/logSlice";
 
 const UPDATE_INTERVAL_MS = 1000 * 60 * 60; // 1000 ms / 60 seconds / 60 minutes = 1x per hour
 
@@ -13,18 +16,36 @@ interface LocQuery {
 }
 
 export const useLocQuery = (): LocQuery => {
+    const dispatch = useAppDispatch();
+
     const [queryType, setQueryType] = useState<TrackerQueryType>("24h");
     const {
         data: coords,
         isLoading,
         isFetching,
         refetch,
+        isError,
+        error,
     } = useGetCoordsQuery(
         {
             type: queryType,
         },
         { pollingInterval: UPDATE_INTERVAL_MS, refetchOnMountOrArgChange: true }
     );
+
+    useEffect(() => {
+        if (isError && error) {
+            // This errors to often to use logError
+            dispatch(logInfo(`useLocQuery failed: ${getErrorMessage(error)}`));
+        }
+    }, [dispatch, error, isError]);
+
+    // There often is an error on the first call, so retry after 5 seconds (also if there is no error)
+    useEffect(() => {
+        setTimeout(() => {
+            refetch();
+        }, 5000);
+    }, [refetch]);
 
     const update = async (): Promise<void> => {
         refetch();
@@ -36,7 +57,7 @@ export const useLocQuery = (): LocQuery => {
     };
 
     return {
-        coords: coords ?? [],
+        coords: coords ?? [[], []],
         update,
         isLoading: isLoading || isFetching,
         toggleQueryType,
